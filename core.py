7aea45037b1d6d45bd70c4b10942fef9b859291a
Tyrence <email1@jkbff.com>
2018-02-08 02:18:57 -0700

    added config file

--- core.py
+++ core.py
@@ -1,5 +1,8 @@
+import json
 from bot import Bot
+
+config = json.load(open("./conf/config.json", "r"))
 
 bot = Bot()
 bot.connect("chat.d1.funcom.com", 7105)
-bot.login("username", "password", "character")
+bot.login(config["username"], config["password"], config["character"])
