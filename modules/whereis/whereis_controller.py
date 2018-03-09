6271e7097ec4e971a4b34c930f85fc0df2d4dea1
Tyrence <email1@jkbff.com>
2018-03-09 01:55:27 -0700

    improved !whereis

--- modules/whereis/whereis_controller.py
+++ modules/whereis/whereis_controller.py
@@ -13,12 +13,14 @@ class WhereisController:
         self.command_manager: CommandManager = registry.get_instance("command_manager")
 
     def start(self):
-        # with open("./modules/whereis/whereis.sql", "r") as f:
-        #    self.db.load_sql(f.read())
-        self.command_manager.register(self.handle_whereis, "whereis", "all", "^(\d+)$")
+        with open("./modules/whereis/whereis.sql", "r") as f:
+            self.db.load_sql(f.read())
+        self.command_manager.register(self.handle_whereis, "whereis", "all", "^(.+)$")
 
     # @command("whereis", "^(.+)$")
     def handle_whereis(self, command, channel, sender, reply, args):
-        # data = self.db.query("SELECT * FROM whereis WHERE name LIKE ?", [args[1]])
-        # reply(data[0]["answer"])
-        reply("hi there")
+        data = self.db.query("SELECT * FROM whereis WHERE name LIKE ?", [args[1]])
+        if len(data) > 0:
+            reply(data[0]["answer"])
+        else:
+            reply("Could not find any results for your search")
