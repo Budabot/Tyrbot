d4a3edf0117733feba8c9eb91b832e36f70ff6b1
Tyrence <email1@jkbff.com>
2018-03-19 02:33:38 -0600

    updated !whereis

--- modules/whereis/whereis_controller.py
+++ modules/whereis/whereis_controller.py
@@ -1,5 +1,6 @@
 from core.decorators import instance, command, event
 from core.db import DB
+from core.text import Text
 from core.chat_blob import ChatBlob
 
 
@@ -10,15 +11,33 @@ class WhereisController:
 
     def inject(self, registry):
         self.db: DB = registry.get_instance("db")
+        self.text: Text = registry.get_instance("text")
 
     def start(self):
         self.db.load_sql_file("./modules/whereis/whereis.sql")
+        self.db.load_sql_file("./modules/whereis/playfields.sql")
 
     @command("whereis", "^(.+)$", "all")
     def handle_whereis_cmd(self, command, channel, sender, reply, args):
         search = args[1]
-        data = self.db.query("SELECT * FROM whereis WHERE name <ENHANCED_LIKE> ?", [search])
-        if len(data) > 0:
-            reply(ChatBlob("Search results for %s" % search, data[0].answer))
+        data = self.db.query("SELECT w.playfield_id, w.name, w.answer, w.xcoord, w.ycoord, p.short_name FROM whereis w "
+                             "LEFT JOIN playfields p ON w.playfield_id = p.id "
+                             "WHERE name <ENHANCED_LIKE> ? OR keywords <ENHANCED_LIKE> ?",
+                             [search, search])
+        count = len(data)
+        if count > 0:
+            blob = ""
+            for row in data:
+                blob += "<header2>" + row.name + "<end>\n" + row.answer
+                if row.playfield_id and row.xcoord and row.ycoord:
+                    blob += " " + self.text.make_chatcmd("waypoint: %sx%s %s" % (row.xcoord, row.ycoord, row.short_name),
+                                                         "/waypoint %s %s %d" % (row.xcoord, row.ycoord, row.playfield_id))
+                blob += "\n\n"
+            reply(ChatBlob("Whereis '%s' (%d)" % (search, count), blob))
         else:
-            reply("Could not find any results for your search")
+            reply("Could not find any results for your search.")
+
+    @event("buddy_logon")
+    def handle_connect_event(self, event_type, event_data):
+        pass
+        # print("handling connect event '%s' for data '%s'" % (event_type, event_data))
