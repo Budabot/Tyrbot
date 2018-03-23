0fd8f45f4b6caaad2013e73075b706917dae5ba2
Tyrence <email1@jkbff.com>
2018-03-22 20:54:51 -0600

    added ConfigController stub

---
+++ core/config/config_controller.py
@@ -0,0 +1,55 @@
+from core.decorators import instance, command, event, timerevent
+from core.db import DB
+from core.text import Text
+from core.chat_blob import ChatBlob
+
+
+@instance()
+class ConfigController:
+    def __init__(self):
+        pass
+
+    def inject(self, registry):
+        self.db: DB = registry.get_instance("db")
+        self.text: Text = registry.get_instance("text")
+
+    def start(self):
+        pass
+
+    @command("config", "^$", "all", "config.txt")
+    def config_list_cmd(self, command, channel, sender, reply, args):
+        sql = """SELECT
+                module,
+                SUM(CASE WHEN enabled = 1 THEN 1 ELSE 0 END) count_enabled,
+                SUM(CASE WHEN enabled = 0 THEN 1 ELSE 0 END) count_disabled
+            FROM
+                (SELECT module, enabled FROM command_config
+                UNION
+                SELECT module, enabled FROM event_config
+                UNION
+                SELECT module, 2 FROM setting) t
+            GROUP BY
+                module
+            ORDER BY
+                module ASC"""
+
+        data = self.db.query(sql)
+        count = len(data)
+        blob = ""
+        current_group = ""
+        for row in data:
+            parts = row.module.split(".")
+            group = parts[0]
+            if group != current_group:
+                current_group = group
+                blob += "\n<header2>" + current_group + "<end>\n"
+
+            blob += self.text.make_chatcmd(row.module, "/tell <myname> config mod " + row.module) + " "
+            if row.count_enabled > 0 and row.count_disabled > 0:
+                blob += "<yellow>Partial<end>"
+            elif row.count_disabled == 0:
+                blob += "<green>Enabled<end>"
+            else:
+                blob += "<red>Disabled<end>"
+            blob += "\n"
+        reply(ChatBlob("Config (%d)" % count, blob))
