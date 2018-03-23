9b22c458c87a3e7c1f9af989363dce95a3f567bb
Tyrence <email1@jkbff.com>
2018-03-22 20:41:23 -0600

    Logger can now log exceptions

--- core/logger.py
+++ core/logger.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import traceback
 
 
 class Logger:
@@ -12,20 +13,26 @@ class Logger:
         self.logger.setLevel("DEBUG")
         self.logger.addHandler(Logger.console_logger)
 
-    def warning(self, msg):
-        self.logger.warning(msg)
+    def warning(self, msg, obj: Exception=None):
+        self.logger.warning(self.format_message(msg, obj))
 
-    def info(self, msg):
-        self.logger.info(msg)
+    def info(self, msg, obj: Exception=None):
+        self.logger.info(self.format_message(msg, obj))
 
-    def error(self, msg):
-        self.logger.error(msg)
+    def error(self, msg, obj: Exception=None):
+        self.logger.error(self.format_message(msg, obj))
 
-    def debug(self, msg):
-        self.logger.debug(msg)
+    def debug(self, msg, obj: Exception=None):
+        self.logger.debug(self.format_message(msg, obj))
 
     def log_chat(self, channel, sender, msg):
         self.info("[%s] %s: %s" % (channel, sender, msg))
 
     def log_tell(self, direction, sender, msg):
         self.info("%s %s: %s" % (direction.capitalize(), sender, msg))
+
+    def format_message(self, msg, obj):
+        if obj:
+            return msg + "\n" + traceback.format_exc()
+        else:
+            return msg
