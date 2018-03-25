e3bdd74a3867611d8f4de9c8ed6b3f44b635e9f2
Tyrence <email1@jkbff.com>
2018-03-25 03:23:06 -0600

    added !help

---
+++ modules/core/help/help_controller.py
@@ -0,0 +1,55 @@
+from core.decorators import instance, command, event
+from core.chat_blob import ChatBlob
+from core.command_params import Text
+
+
+@instance()
+class HelpController:
+    def __init__(self):
+        pass
+
+    def inject(self, registry):
+        self.text = registry.get_instance("text")
+        self.db = registry.get_instance("db")
+        self.access_manager = registry.get_instance("access_manager")
+        self.command_manager = registry.get_instance("command_manager")
+
+    def start(self):
+        pass
+
+    @command(command="help", params=[], access_level="all",
+             description="Show a list of commands to get help with")
+    def help_list_cmd(self, command_str, channel, sender, reply, args):
+        data = self.db.query("SELECT command, module, access_level FROM command_config WHERE channel = ? "
+                             "ORDER BY module ASC", [channel])
+        blob = ""
+        current_group = ""
+        current_module = ""
+        for row in data:
+            if not self.access_manager.check_access(sender, row.access_level):
+                continue
+
+            parts = row.module.split(".")
+            group = parts[0]
+            module = parts[1]
+            if group != current_group:
+                current_group = group
+                blob += "\n\n<header2>" + current_group + "<end>"
+
+            if module != current_module:
+                current_module = module
+                blob += "\n" + module + ":"
+
+            blob += " " + self.text.make_chatcmd(row.command, "/tell <myname> help " + row.command)
+
+        reply(ChatBlob("Help (main)", blob))
+
+    @command(command="help", params=[Text("command")], access_level="all",
+             description="Show help for a specific command")
+    def help_detail_cmd(self, command_str, channel, sender, reply, args):
+        help_topic = args[1]
+        help_text = self.command_manager.get_help_text(sender, help_topic, channel)
+        if help_text:
+            reply(help_text)
+        else:
+            reply("Could not find help on <highlight>" + help_topic + "<end>.")
