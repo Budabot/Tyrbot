17cc77935fd74816289136846a6606b5b2f2713a
Tyrence <email1@jkbff.com>
2018-04-01 17:10:46 -0600

    added !checkaccess

--- modules/core/system/system_controller.py
+++ modules/core/system/system_controller.py
@@ -1,4 +1,5 @@
 from core.decorators import instance, command
+from core.commands.param_types import Any
 
 
 @instance()
@@ -8,6 +9,7 @@ class SystemController:
 
     def inject(self, registry):
         self.bot = registry.get_instance("budabot")
+        self.access_manager = registry.get_instance("access_manager")
 
     def start(self):
         pass
@@ -23,3 +25,10 @@ class SystemController:
     def restart_cmd(self, channel, sender, reply, args):
         reply("Restarting down the bot...")
         self.bot.restart()
+
+    @command(command="checkaccess", params=[Any("character")], access_level="all",
+             description="Check access level for a character")
+    def checkaccess_cmd(self, channel, sender, reply, args):
+        char = args[1].capitalize()
+        access_level = self.access_manager.get_access_level(char)
+        reply("Access level for <highlight>%s<end> is <highlight>%s<end>." % (char, access_level["label"]))
