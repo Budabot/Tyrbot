00f1a07707b46f550fe24965ea57a7cfc3d9a0df
Tyrence <email1@jkbff.com>
2018-04-07 22:02:24 -0600

    added !alts, !alts add, and !alts remove commands

---
+++ core/alts/alts_manager.py
@@ -0,0 +1,75 @@
+from core.decorators import instance
+import os
+
+
+@instance()
+class AltsManager:
+    UNVALIDATED = 0
+    VALIDATED = 1
+    MAIN = 2
+
+    def __init__(self):
+        pass
+
+    def inject(self, registry):
+        self.db = registry.get_instance("db")
+        self.character_manager = registry.get_instance("character_manager")
+        self.pork_manager = registry.get_instance("pork_manager")
+
+    def start(self):
+        self.db.load_sql_file("alts.sql", os.path.dirname(__file__))
+
+    def get_alts(self, char):
+        char_id = self.character_manager.resolve_char_to_id(char)
+        sql = "SELECT c.* FROM character c " \
+              "LEFT JOIN alts a ON c.char_id = a.char_id AND a.status >= ?" \
+              "WHERE c.char_id = ? OR a.group_id = (" \
+              "SELECT group_id FROM alts WHERE char_id = ?) " \
+              "ORDER BY a.status DESC"
+
+        return self.db.query(sql, [self.VALIDATED, char_id, char_id])
+
+    def add_alt(self, sender_char_id, alt_char_id):
+        alt_row = self.get_alt_status(alt_char_id)
+        if alt_row:
+            return False
+
+        sender_row = self.get_alt_status(sender_char_id)
+        if sender_row:
+            if sender_row.status == self.MAIN or sender_row.status == self.VALIDATED:
+                params = [alt_char_id, sender_row.group_id, self.VALIDATED]
+            else:
+                params = [alt_char_id, sender_row.group_id, self.UNVALIDATED]
+        else:
+            # main does not exist, create entry for it
+            group_id = self.get_next_group_id()
+            self.db.exec("INSERT INTO alts (char_id, group_id, status) VALUES (?, ?, ?)",
+                         [sender_char_id, group_id, self.MAIN])
+
+            params = [alt_char_id, group_id, self.VALIDATED]
+
+        self.pork_manager.get_character_info(alt_char_id)
+        self.db.exec("INSERT INTO alts (char_id, group_id, status) VALUES (?, ?, ?)", params)
+        return True
+
+    def remove_alt(self, sender_char_id, alt_char_id):
+        alt_row = self.get_alt_status(alt_char_id)
+        sender_row = self.get_alt_status(sender_char_id)
+
+        # sender and alt do not belong to the same group id
+        if not alt_row or not sender_row or alt_row.group_id != sender_row.group_id:
+            return False
+
+        # cannot remove alt from an unvalidated sender
+        if sender_row.status == self.UNVALIDATED:
+            return False
+
+        self.db.exec("DELETE FROM alts WHERE char_id = ?", [alt_char_id])
+        return True
+
+    def get_alt_status(self, char_id):
+        return self.db.query_single("SELECT group_id, status FROM alts WHERE char_id = ?", [char_id])
+
+    def get_next_group_id(self):
+        row = self.db.query_single("SELECT (IFNULL(MAX(group_id), 1) + 1) AS next_group_id FROM alts")
+        return row.next_group_id
---
+++ modules/core/alts/alts_controller.py
@@ -0,0 +1,53 @@
+from core.decorators import instance, command
+from core.commands.param_types import Any, Const, Options
+from core.chat_blob import ChatBlob
+
+
+@instance()
+class AltsController:
+    def __init__(self):
+        pass
+
+    def inject(self, registry):
+        self.alts_manager = registry.get_instance("alts_manager")
+        self.character_manager = registry.get_instance("character_manager")
+
+    def start(self):
+        pass
+
+    @command(command="alts", params=[], access_level="all",
+             description="Show your alts")
+    def alts_list_cmd(self, channel, sender, reply, args):
+        alts = self.alts_manager.get_alts(sender.char_id)
+        blob = ""
+        for alt in alts:
+            blob += "<highlight>%s<end> (%d/<green>%d<end>) %s %s\n" %\
+                  (alt.name, alt.level, alt.ai_level, alt.faction, alt.profession)
+
+        reply(ChatBlob("Alts for %s (%d)" % (sender.name, len(alts)), blob))
+
+    @command(command="alts", params=[Const("add"), Any("character")], access_level="all",
+             description="Add an alt")
+    def alts_add_cmd(self, channel, sender, reply, args):
+        alt = args[1].capitalize()
+        alt_char_id = self.character_manager.resolve_char_to_id(alt)
+
+        if not alt_char_id:
+            reply("Could not find character <highlight>%s<end>." % alt)
+        elif self.alts_manager.add_alt(sender.char_id, alt_char_id):
+            reply("<highlight>%s<end> added as alt successfully." % alt)
+        else:
+            reply("Could not add <highlight>%s<end> as alt." % alt)
+
+    @command(command="alts", params=[Options(["rem", "remove"]), Any("character")], access_level="all",
+             description="Remove an alt")
+    def alts_remove_cmd(self, channel, sender, reply, args):
+        alt = args[2].capitalize()
+        alt_char_id = self.character_manager.resolve_char_to_id(alt)
+
+        if not alt_char_id:
+            reply("Could not find character <highlight>%s<end>." % alt)
+        elif self.alts_manager.remove_alt(sender.char_id, alt_char_id):
+            reply("<highlight>%s<end> removed as alt successfully." % alt)
+        else:
+            reply("Could not remove <highlight>%s<end> as alt." % alt)
