1ff0bcb7f386b8cf013e83f95da3f36730534227
Ilon Sjögren <ilon.sjogren@viaplay.com>
2018-04-13 10:31:14 +0200

    Comment should make clear when to restart and exit

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -o pipefail -o errexit
 
-# The bot are using non-zero exit codes to signal state
-# Exit on a non-zero exit code.
-# This should be changed.
+# The bot are using non-zero exit codes to signal state.
+# Should be restarted until it returns a exit code of zero.
 while true; do
     python3 bootstrap.py && exit
 done
