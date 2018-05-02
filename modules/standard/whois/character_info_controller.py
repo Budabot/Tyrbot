c992f4d65e90d02dd595b54d4f5bb4ef511a00a5
Tyrence <email1@jkbff.com>
2018-05-02 01:55:09 -0600

    added extended info for !whois

--- modules/standard/whois/character_info_controller.py
+++ modules/standard/whois/character_info_controller.py
@@ -20,11 +20,30 @@ class CharacterInfoController:
              description="Get whois information for a character")
     def whois_cmd(self, channel, sender, reply, args):
         char_name = args[1].capitalize()
-        whois = self.pork_manager.get_character_info(char_name)
+        char_info = self.pork_manager.get_character_info(char_name)
         char_id = self.character_manager.resolve_char_to_id(char_name)
-        if whois:
-            # TODO add extended info
-            msg = "<highlight>%s<end> (%d/<green>%d<end>) %s %s" % (whois.name, whois.level, whois.ai_level, whois.faction, whois.profession)
+        if char_info:
+            blob = "Name: %s\n" % self.get_full_name(char_info)
+            blob += "Profession: %s\n" % char_info.profession
+            blob += "Faction: %s\n" % char_info.faction
+            blob += "Breed: %s\n" % char_info.breed
+            blob += "Gender: %s\n" % char_info.gender
+            blob += "Level: %d\n" % char_info.level
+            blob += "AI Level: %d\n" % char_info.ai_level
+            if char_info.org_id:
+                blob += "Org: %s (%d)\n" % (char_info.org_name, char_info.org_id)
+                blob += "Org Rank: %s (%d)\n" % (char_info.org_rank_name, char_info.org_rank_id)
+            else:
+                blob += "Org: &lt;None&gt;\n"
+                blob += "Org Rank: &lt;None&gt;\n"
+            blob += "Head Id: %d\n" % char_info.head_id
+            blob += "PVP Rating: %d" % char_info.pvp_rating
+            blob += "PVP Title: %s\n" % char_info.pvp_title
+            blob += "Character Id: %d\n" % char_info.char_id
+            blob += "Source: %s\n" % char_info.source
+            more_info = self.text.paginate("More Info", blob, 5000, 1)[0]
+
+            msg = "<highlight>%s<end> (%d/<green>%d<end>) %s %s %s" % (char_info.name, char_info.level, char_info.ai_level, char_info.faction, char_info.profession, more_info)
             reply(msg)
         elif char_id:
             blob = "<notice>Note: Could not retrieve detailed info for character.<end>\n\n"
@@ -33,3 +52,15 @@ class CharacterInfoController:
             reply(ChatBlob("Basic Info for %s" % char_name, blob))
         else:
             reply("Could not find info for character <highlight>%s<end>." % char_name)
+
+    def get_full_name(self, char_info):
+        name = ""
+        if char_info.first_name:
+            name += char_info.first_name + " "
+
+        name += "\"" + char_info.name + "\""
+
+        if char_info.last_name:
+            name += " " + char_info.last_name
+
+        return name
