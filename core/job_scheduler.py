22234a91d74f2d749c8cedd97e8c35b227f1ace0
Tyrence <email1@jkbff.com>
2018-05-04 00:08:26 -0600

    JobScheduler can now cancel jobs

--- core/job_scheduler.py
+++ core/job_scheduler.py
@@ -1,6 +1,5 @@
 from core.decorators import instance
 from core.logger import Logger
-from collections import deque
 import time
 
 
@@ -8,7 +7,7 @@ import time
 class JobScheduler:
     def __init__(self):
         self.logger = Logger("setting_manager")
-        self.scheduled_jobs = deque([])
+        self.jobs = []
         self.job_id_index = 0
 
     def inject(self, registry):
@@ -18,9 +17,9 @@ class JobScheduler:
         pass
 
     def check_for_scheduled_jobs(self, timestamp):
-        while self.scheduled_jobs and self.scheduled_jobs[0]["time"] <= timestamp:
+        while self.jobs and self.jobs[0]["time"] <= timestamp:
             try:
-                job = self.scheduled_jobs.popleft()
+                job = self.jobs.pop(0)
                 job["callback"](job["time"], *job["args"], **job["kwargs"])
             except Exception as e:
                 self.logger.warning("Error processing scheduled job", e)
@@ -38,12 +37,18 @@ class JobScheduler:
         self._insert_job(new_job)
         return job_id
 
+    def cancel_job(self, job_id):
+        for index, job in enumerate(self.jobs):
+            if job["id"] == job_id:
+                return self.jobs.pop(index)
+        return None
+
     def _insert_job(self, new_job):
-        for index, job in enumerate(self.scheduled_jobs):
+        for index, job in enumerate(self.jobs):
             if job["time"] > new_job["time"]:
-                self.scheduled_jobs.insert(index, new_job)
+                self.jobs.insert(index, new_job)
                 return
-        self.scheduled_jobs.append(new_job)
+        self.jobs.append(new_job)
 
     def _get_next_job_id(self):
         self.job_id_index += 1
