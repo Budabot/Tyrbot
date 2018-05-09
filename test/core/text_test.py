eba29963d3f1e36c3551b276660e8f298888a5d3
Tyrence <email1@jkbff.com>
2018-05-09 03:31:05 -0600

    added tests

---
+++ test/core/text_test.py
@@ -0,0 +1,32 @@
+from core.text import Text
+import unittest
+from unittest.mock import Mock, MagicMock
+
+
+class TestTest(unittest.TestCase):
+
+    def test_get_next_line(self):
+        msg = "hello this is a test\nthis is another test as well\nand a third\ntest also\nwhich is\nshort"
+        text = Text()
+
+        self.assertEqual(text.get_next_line(msg, {"symbol": "\n", "include": True})[0], 'hello this is a test\n')
+        self.assertEqual(text.get_next_line(msg, {"symbol": " ", "include": False})[0], 'hello')
+
+    def test_paginate(self):
+        setting = Mock()
+        setting.get_value = MagicMock(return_value="test")
+        setting_manager = Mock()
+        setting_manager.get = MagicMock(return_value=setting)
+
+        bot = Mock()
+        bot.char_name = "char_name"
+        bot.org_name = "org_name"
+
+        text = Text()
+        text.setting_manager = setting_manager
+        text.bot = bot
+
+        msg = "hello this is a test\nthis is another test as well\nand a third\ntest also\nwhich is\nshort"
+        pages = text.paginate("label", msg, 115)
+        self.assertEqual(len(pages), 2)
+        self.assertTrue("text://short" in pages[1])
---
+++ test/core/util_test.py
@@ -0,0 +1,27 @@
+import unittest
+from core.util import Util
+
+
+class UtilTest(unittest.TestCase):
+
+    def test_time_to_readable(self):
+        util = Util()
+        self.assertEqual(util.time_to_readable(0), '0 secs')
+        self.assertEqual(util.time_to_readable(1), '1 sec')
+        self.assertEqual(util.time_to_readable(59), '59 secs')
+        self.assertEqual(util.time_to_readable(60), '1 min')
+        self.assertEqual(util.time_to_readable(3600), '1 hr')
+        self.assertEqual(util.time_to_readable(604800), '7 days')
+        self.assertEqual(util.time_to_readable(604800, max_unit="week"), '1 week')
+        self.assertEqual(util.time_to_readable(604800, max_unit="hr"), '168 hrs')
+        self.assertEqual(util.time_to_readable(86410), '1 day')
+        self.assertEqual(util.time_to_readable(86410, max_levels=4), '1 day 10 secs')
+
+    def test_parse_time(self):
+        util = Util()
+        self.assertEqual(util.parse_time("10s"), 10)
+        self.assertEqual(util.parse_time("10s sdfsd", "hola"), "hola")
+        self.assertEqual(util.parse_time("1m10s"), 70)
+        self.assertEqual(util.parse_time("10s1m"), 70)
+        self.assertEqual(util.parse_time("10s1m"), 70)
+        self.assertEqual(util.parse_time("1s3hr2d1m"), 183661)
