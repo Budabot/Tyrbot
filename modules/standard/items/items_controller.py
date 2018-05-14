a6e3d060400365cd82f7daf8552c9c7ae13b9193
Tyrence <email1@jkbff.com>
2018-05-14 12:34:11 -0600

    added stub for !items

---
+++ modules/standard/items/items_controller.py
@@ -0,0 +1,28 @@
+from core.decorators import instance, command
+from core.db import DB
+from core.text import Text
+from core.chat_blob import ChatBlob
+from core.command_param_types import Int, Any
+import os
+
+
+@instance()
+class ItemsController:
+    def __init__(self):
+        pass
+
+    def inject(self, registry):
+        self.db: DB = registry.get_instance("db")
+        self.text: Text = registry.get_instance("text")
+
+    def start(self):
+        self.db.load_sql_file("aodb.sql", os.path.dirname(__file__))
+
+    @command(command="items", params=[Int("ql", is_optional=True), Any("search")], access_level="all",
+             description="Search for an item")
+    def items_cmd(self, channel, sender, reply, args):
+        ql = args[0]
+        search = args[1]
+
+        count = 0
+        reply(ChatBlob("Item Search Results (%d)" % count, str(ql) + " " + search))
