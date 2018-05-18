2ead4525a7bba3d0c630fd7c681d40fc76af2f1d
Tyrence <email1@jkbff.com>
2018-05-18 01:18:18 -0600

    added list of mmdb values

---
+++ core/aochat/extended_message.py
@@ -0,0 +1,9 @@
+class ExtendedMessage:
+    def __init__(self, category_id, instance_id, template, params):
+        self.category_id = category_id
+        self.instance_id = instance_id
+        self.template = template
+        self.params = params
+
+    def get_message(self):
+        return self.template % self.params
--- core/aochat/mmdb_parser.py
+++ core/aochat/mmdb_parser.py
@@ -1,3 +1,6 @@
+import struct
+
+
 class MMDBParser:
     def __init__(self, filename):
         self.filename = filename
@@ -21,6 +24,31 @@ class MMDBParser:
                 return self.read_string(file)
             else:
                 return None
+
+    def get_all_message_strings(self):
+        with open(self.filename, "rb") as file:
+            categories = iter(list(self.get_categories(file)))
+            next_category = next(categories)
+
+            while True:
+                try:
+                    category = next_category
+                    next_category = next(categories)
+                except StopIteration:
+                    break
+
+                max_offset = next_category["offset"]
+                file.seek(category["offset"])
+
+                instances = []
+                while file.tell() < max_offset:
+                    entry = self.read_entry(file)
+                    instances.append(entry)
+
+                for instance in instances:
+                    file.seek(instance["offset"])
+                    message_string = self.read_string(file)
+                    print([category["id"], instance["id"], message_string])
 
     def find_entry(self, file, entry_id, min_offset, max_offset):
         file.seek(min_offset)
@@ -53,4 +81,52 @@ class MMDBParser:
             message.append(ord(char))
             char = file.read(1)
 
-        return message.decode("utf-8")
+        return message.decode("ISO-8859-1")
+
+    def read_base_85(self, num_str):
+        n = 0
+        for i in range(0, 5):
+            n = n * 85 + ord(num_str[i]) - 33
+        return n
+
+    def parse_params(self, param_str):
+        args = []
+        while param_str:
+            data_type = param_str[0]
+            param_str = param_str[1:]
+            if data_type == "S":
+                size = ord(param_str[0]) * 256 + ord(param_str[1])
+                args.append(param_str[2:2 + size])
+                param_str = param_str[2 + size:]
+            elif data_type == "s":
+                size = ord(param_str[0])
+                args.append(param_str[1:1 + size])
+                param_str = param_str[1 + size:]
+            elif data_type == "I":
+                args.append(struct.unpack(">I", param_str[:4])[0])
+                param_str = param_str[4:]
+            elif data_type == "i" or data_type == "u":
+                args.append(self.read_base_85(param_str[:5]))
+                param_str = param_str[5:]
+            elif data_type == "R":
+                category_id = self.read_base_85(param_str[:5])
+                instance_id = self.read_base_85(param_str[5:10])
+                message = self.get_message_string(category_id, instance_id)
+                if not message:
+                    raise Exception("Could not find message string for category '%s' and instance '%s'" % (category_id, instance_id))
+                args.append(message)
+                param_str = param_str[10:]
+            elif data_type == "l":
+                category_id = 20000
+                instance_id = struct.unpack(">I", param_str[:4])[0]
+                message = self.get_message_string(category_id, instance_id)
+                if not message:
+                    raise Exception("Could not find message string for category '%s' and instance '%s'" % (category_id, instance_id))
+                args.append(message)
+                param_str = param_str[5:]
+            elif data_type == "~":
+                break
+            else:
+                raise Exception("Unknown argument type '%s'" % data_type)
+
+        return args
