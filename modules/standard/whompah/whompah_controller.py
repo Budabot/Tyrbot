f27fc00ca22eae6bed45cbf309953c8f84d7143c
Tyrence <email1@jkbff.com>
2018-07-03 19:06:03 -0600

    added !whompah travel

--- modules/standard/whompah/whompah_controller.py
+++ modules/standard/whompah/whompah_controller.py
@@ -1,7 +1,7 @@
 from core.decorators import instance, command
 from core.db import DB
 from core.text import Text
-from core.command_param_types import Any, Int
+from core.command_param_types import Any
 from core.chat_blob import ChatBlob
 
 
@@ -29,7 +29,29 @@ class WhompahController:
     @command(command="whompah", params=[Any("city1"), Any("city2")], access_level="all",
              description="Show whompah route between two cities")
     def whompah_travel_cmd(self, channel, sender, reply, args):
-        pass
+        city_name1 = args[0]
+        city_name2 = args[1]
+        city1 = self.get_whompah_city(city_name1)
+        city2 = self.get_whompah_city(city_name2)
+
+        if not city1:
+            reply("Could not find whompah city <highlight>%s<end>." % city_name1)
+            return
+        elif not city2:
+            reply("Could not find whompah city <highlight>%s<end>." % city_name2)
+            return
+
+        data = self.db.query("SELECT w1.*, w2.city2_id AS city_rel FROM whompah_cities w1 JOIN whompah_cities_rel w2 ON w1.id = w2.city1_id")
+
+        cities = {}
+        for city in data:
+            rel = cities.get(city.id, {}).get("rel", [])
+            rel.append(city.city_rel)
+            cities[city.id] = city.row
+            cities[city.id]["rel"] = rel
+
+        path = self.format_path(self.find_path(cities, city1.id, city2.id))
+        reply(" -> ".join(path))
 
     @command(command="whompah", params=[Any("city")], access_level="all",
              description="Show whompah destinations for a city")
@@ -48,3 +70,36 @@ class WhompahController:
 
     def get_whompah_city(self, city):
         return self.db.query_single("SELECT id, city_name, zone, faction, short_name FROM whompah_cities WHERE city_name LIKE ? OR short_name LIKE ?", [city, city])
+
+    def find_path(self, cities, start_city_id, end_city_id):
+        def get_and_remove(key):
+            value = cities[key]
+            del cities[key]
+            return value
+
+        end_city = get_and_remove(end_city_id)
+
+        if start_city_id == end_city_id:
+            return end_city
+
+        # create stack and initialize
+        # start with ending city and traverse backwards so we don't have to reverse the result
+        stack = [end_city]
+        while stack:
+            root = stack.pop(0)
+            for rel in root["rel"]:
+                if rel in cities:
+                    c = get_and_remove(rel)
+                    c["parent"] = root
+                    if c["id"] == start_city_id:
+                        return c
+                    stack.append(c)
+        return None
+
+    def format_path(self, path):
+        result = []
+        root = path
+        while root:
+            result.append(root["city_name"])
+            root = root.get("parent")
+        return result
