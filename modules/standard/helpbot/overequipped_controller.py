558313c8889cf9829e0effffd07c4b735d300de3
Tyrence <email1@jkbff.com>
2018-07-04 13:23:16 -0600

    added !dyna

--- modules/standard/helpbot/overequipped_controller.py
+++ modules/standard/helpbot/overequipped_controller.py
@@ -8,15 +8,9 @@ import math
 
 @instance()
 class OverequippedController:
-    def __init__(self):
-        pass
-
     def inject(self, registry):
         self.db: DB = registry.get_instance("db")
         self.text: Text = registry.get_instance("text")
-
-    def start(self):
-        pass
 
     @command(command="oe", params=[Int("skill_level")], access_level="all",
              description="Show the current time in every timezone")
@@ -31,14 +25,14 @@ class OverequippedController:
         oe25 = int(math.floor(skill_level / 0.2))
         oe25low = int(math.floor(skill_level * 0.2))
 
-        blob = "With a skill requirement of <higlight>%s<end>, you will be\n" % skill_level
+        blob = "With a skill requirement of <highlight>%s<end>, you will be\n" % skill_level
         blob += "Out of OE: <highlight>%d<end> or higher\n" % oe100low
         blob += "75%%: <highlight>%d<end> to <highlight>%d<end>\n" % (oe75low, oe100low - 1)
         blob += "50%%: <highlight>%d<end> to <highlight>%d<end>\n" % (oe50low, oe75low - 1)
         blob += "25%%: <highlight>%d<end> to <highlight>%d<end>\n" % (oe25low, oe50low - 1)
         blob += "0%%: <highlight>%d<end> or lower\n\n" % (oe25low - 1)
 
-        blob += "With a personal skill of <higlight>%s<end>, you can use up to\n" % skill_level
+        blob += "With a personal skill of <highlight>%s<end>, you can use up to\n" % skill_level
         blob += "Out of OE: <highlight>%d<end> or lower\n" % oe100
         blob += "75%%: <highlight>%d<end> to <highlight>%d<end>\n" % (oe100 + 1, oe75)
         blob += "50%%: <highlight>%d<end> to <highlight>%d<end>\n" % (oe75 + 1, oe50)
