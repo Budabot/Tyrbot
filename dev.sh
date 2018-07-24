632089641039af280ee262c7e4a9a5e92dee5cb1
Tyrence <email1@jkbff.com>
2018-07-24 15:17:50 -0600

    code cleanup

--- dev.sh
+++ dev.sh
@@ -7,7 +7,7 @@ pip install -U -r requirements.txt
 
 set -o pipefail -o errexit
 
-# The bot are using non-zero exit codes to signal state.
+# The bot uses non-zero exit codes to signal state.
 # Should be restarted until it returns an exit code of zero.
 while true; do
     git pull
--- start.bat
+++ start.bat
@@ -4,7 +4,11 @@ pip install -U -r requirements.txt
 
 :start
 python ./bootstrap.py
+
+REM The bot uses non-zero exit codes to signal state.
+REM Should be restarted until it returns an exit code of zero.
 if %errorlevel% == 0 goto end
+
 timeout /t 1
 goto :start
 
