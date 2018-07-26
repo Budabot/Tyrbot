103446900186ecacb178b692e02d24eeacd2e0a9
Tyrence <email1@jkbff.com>
2018-07-25 19:41:59 -0600

    added !whatbuffs

---
+++ modules/standard/items/what_buffs_controller.py
@@ -0,0 +1,118 @@
+from core.decorators import instance, command
+from core.db import DB
+from core.text import Text
+from core.chat_blob import ChatBlob
+from core.command_param_types import Any, Options
+
+
+@instance()
+class WhatBuffsController:
+    def inject(self, registry):
+        self.db: DB = registry.get_instance("db")
+        self.text: Text = registry.get_instance("text")
+
+    @command(command="whatbuffs", params=[], access_level="all",
+             description="Find items or nanos that buff a skill (or ability)")
+    def whatbuffs_list_cmd(self, channel, sender, reply, args):
+        data = self.db.query("SELECT name FROM skills ORDER BY name ASC")
+        blob = ""
+        for row in data:
+            blob += self.text.make_chatcmd(row.name, "/tell <myname> whatbuffs %s" % row.name) + "\n"
+
+        blob += self.get_footer()
+        reply(ChatBlob("Whatbuffs Skill List", blob))
+
+    @command(command="whatbuffs", params=[
+        Options(["arms", "back", "chest", "deck", "feet", "fingers", "hands", "head", "hud", "legs", "nano", "neck", "shoulders", "unknown", "util", "weapon", "wrists"]),
+        Any("skill")], access_level="all",
+             description="Find items or nanos that buff a skill (or ability)")
+    def whatbuffs_detail_cmd(self, channel, sender, reply, args):
+        item_type = args[0].capitalize()
+        skill_name = args[1]
+
+        reply(self.show_search_results(item_type, skill_name))
+
+    @command(command="whatbuffs", params=[Any("skill")], access_level="all",
+             description="Find items or nanos that buff a skill (or ability)")
+    def whatbuffs_skill_cmd(self, channel, sender, reply, args):
+        skill_name = args[0]
+
+        skills = self.search_for_skill(skill_name)
+        if len(skills) == 0:
+            reply("Could not find skill <highlight>%s<end>." % skill_name)
+        elif len(skills) == 1:
+            skill = skills.pop()
+
+            data = self.db.query("SELECT i.item_type, COUNT(1) AS cnt "
+                                 "FROM aodb "
+                                 "JOIN item_types i ON aodb.highid = i.item_id "
+                                 "JOIN item_buffs b ON aodb.highid = b.item_id "
+                                 "JOIN skills s ON b.attribute_id = s.id "
+                                 "WHERE s.id = ? "
+                                 "GROUP BY item_type "
+                                 "HAVING cnt > 0 "
+                                 "ORDER BY item_type ASC", [skill.id])
+
+            blob = ""
+            for row in data:
+                blob += "%s (%d)\n" % (self.text.make_chatcmd(row.item_type, "/tell <myname> whatbuffs %s %s" % (row.item_type, skill.name)), row.cnt)
+
+            blob += self.get_footer()
+            reply(ChatBlob("Whatbuffs %s - Choose Type" % skill.name, blob))
+        else:
+            blob = "Choose a skill:\n\n"
+            for skill in skills:
+                blob += self.text.make_chatcmd(skill.name, "/tell <myname> whatbuffs %s" % skill.name) + "\n"
+
+            blob += self.get_footer()
+            reply(ChatBlob("Whatbuffs - Choose Skill", blob))
+
+    def search_for_skill(self, skill_name):
+        skill_name = skill_name.lower()
+
+        data = self.db.query(*self.db.handle_extended_like("SELECT id, name FROM skills WHERE name <EXTENDED_LIKE=0> ?", [skill_name]))
+
+        # check for exact match first, in order to disambiguate between Bow and Bot Special Attack
+        for row in data:
+            if row.name.lower() == skill_name:
+                return [row]
+
+        return data
+
+    def show_search_results(self, item_type, skill_name):
+        skills = self.search_for_skill(skill_name)
+
+        if len(skills) == 0:
+            return "Could not find skill <highlight>%s<end>." % skill_name
+        elif len(skills) == 1:
+            skill = skills.pop()
+            return self.get_search_results(item_type, skill)
+        else:
+            blob = ""
+            for skill in skills:
+                blob += self.text.make_chatcmd(skill.name, "/tell <myname> whatbuffs %s %s" % (item_type, skill.name)) + "\n"
+
+            return ChatBlob("Whatbuffs - Choose Skill", blob)
+
+    def get_search_results(self, item_type, skill):
+        data = self.db.query("SELECT aodb.*, b.amount "
+                             "FROM aodb "
+                             "JOIN item_types i ON aodb.highid = i.item_id "
+                             "JOIN item_buffs b ON aodb.highid = b.item_id "
+                             "JOIN skills s ON b.attribute_id = s.id "
+                             "WHERE i.item_type LIKE ? AND s.id = ? "
+                             "ORDER BY amount DESC", [item_type, skill.id])
+
+        if len(data) == 0:
+            return "No items found of type <highlight>%s<end> that buff <highlight>%s<end>." % (item_type, skill.name)
+        else:
+            blob = ""
+            for row in data:
+                blob += "%s (%d)\n" % (self.text.make_item(row.lowid, row.highid, row.highql, row.name), row.amount)
+
+            blob += self.get_footer()
+
+            return ChatBlob("Whatbuffs - %s %s (%d)" % (item_type, skill.name, len(data)), blob)
+
+    def get_footer(self):
+        return "\nItem DB Extraction Info provided by Unk"
