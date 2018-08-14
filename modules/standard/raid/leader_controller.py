7bb6c7fc98a03bc3eaacca74a0975e8979e7dd4f
KristinHansen <maagenator@gmail.com>
2018-08-14 23:20:57 +0200

    added leader logic (auto remove, leader acitivity etc)

--- modules/standard/raid/leader_controller.py
+++ modules/standard/raid/leader_controller.py
@@ -1,19 +1,25 @@
 from core.command_param_types import Const, Any, Character
 from core.db import DB
-from core.decorators import instance, command
+from core.decorators import instance, command, timerevent, event
 from core.text import Text
+from core.tyrbot import Tyrbot
+from core.private_channel_service import PrivateChannelService
+from modules.core.org_members.org_member_controller import OrgMemberController
+import time
 
 
 @instance()
 class LeaderController:
     def __init__(self):
         self.leader = None
+        self.last_activity = None
 
     def inject(self, registry):
         self.db: DB = registry.get_instance("db")
         self.text: Text = registry.get_instance("text")
         self.access_service = registry.get_instance("access_service")
         self.character_service = registry.get_instance("character_service")
+        self.bot: Tyrbot = registry.get_instance("bot")
 
     @command(command="leader", params=[], access_level="all",
              description="Show the current raidleader")
@@ -49,3 +55,39 @@ class LeaderController:
             return "<highlight>%s<end> has been set as raidleader." % char.name
         else:
             return "You do not have a high enough access level to take raidleader from <highlight>%s<end>." % self.character_service.resolve_char_to_name(self.leader)
+
+    @timerevent(budatime="1h", description="Remove leader if leader hasn't been active for more than 1 hour")
+    def leader_auto_remove(self, event_type, event_data):
+        if self.last_activity:
+            if self.last_activity - int(time.time()) > 3600:
+                self.leader = None
+                self.last_activity = None
+
+                self.bot.send_private_channel_message("Leader has been automatically cleared because of inactivity.")
+                self.bot.send_org_message("Leader has been automatically cleared because of inactivity.")
+
+    @event(PrivateChannelService.LEFT_PRIVATE_CHANNEL_EVENT, "Remove leader if leader leaves private channel")
+    def leader_remove_on_leave_private(self, event_type, event_data):
+        if self.leader == event_data.char_id:
+            self.leader = None
+            self.last_activity = None
+            self.bot.send_private_channel_message("%s left private channel, and has been cleared as leader." % self.character_service.resolve_char_to_name(event_data.char_id))
+
+    @event(OrgMemberController.ORG_MEMBER_LOGOFF_EVENT, "Remove leader if leader logs off")
+    def leader_remove_on_logoff(self, event_type, event_data):
+        if self.leader == event_data.char_id:
+            self.leader = None
+            self.last_activity = None
+            self.bot.send_org_message("%s has logged off, and has been cleared as leader." % self.character_service.resolve_char_to_name(event_data.char_id))
+
+    def activity_done(self):
+        self.last_activity = int(time.time())
+
+    def can_use_command(self, char_id):
+        self.leader
+
+        if not self.leader or self.leader == char_id or self.access_service.has_sufficient_access_level(char_id, self.leader):
+            self.activity_done()
+            return True
+
+        return False
--- modules/standard/raid/raid_controller.py
+++ modules/standard/raid/raid_controller.py
@@ -1,9 +1,11 @@
+from core.access_service import AccessService
 from core.chat_blob import ChatBlob
 from core.command_param_types import Const, Int, Item, Any
 from core.db import DB
 from core.decorators import instance, command, timerevent
 from core.text import Text
 from core.tyrbot import Tyrbot
+from .leader_controller import LeaderController
 from collections import OrderedDict
 import secrets
 import time
@@ -36,6 +38,8 @@ class RaidController:
         self.bot: Tyrbot = registry.get_instance("bot")
         self.db: DB = registry.get_instance("db")
         self.text: Text = registry.get_instance("text")
+        self.leader_controller: LeaderController = registry.get_instance("leader_controller")
+        self.access_service: AccessService = registry.get_instance("access_service")
 
     @command(command="loot", params=[], description="Show the list of added items", access_level="all")
     def loot_cmd(self, request):
@@ -66,22 +70,29 @@ class RaidController:
 
         return ChatBlob("Loot (%d)" % len(self.loot_list), blob)
 
-    @command(command="loot", params=[Const("clear")], description="Clear all loot", access_level="member")
+    @command(command="loot", params=[Const("clear")], description="Clear all loot", access_level="all")
     def loot_clear_cmd(self, request, _):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         if self.loot_list:
             self.loot_list.clear()
+            self.last_modify = None
             return "Loot list cleared."
         else:
             return "Loot list is already empty."
 
     @command(command="loot", params=[Const("remitem"), Int("item_index")], description="Remove existing loot", access_level="all")
     def loot_rem_item_cmd(self, request, _, item_index):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         if not self.loot_list:
             return "Loot list is empty."
 
         try:
             if self.loot_list[item_index]:
-                self.last_modify = int(float(time.time()))
+                self.last_modify = int(time.time())
                 return "Removed %s from loot list." % self.loot_list.pop(item_index).item.name
             else:
                 return "Item error."
@@ -90,6 +101,9 @@ class RaidController:
 
     @command(command="loot", params=[Const("additem"), Item("item"), Int("item_count", is_optional=True)], description="Add an item to loot list", access_level="all")
     def loot_add_item_cmd(self, request, _, item, item_count):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         if item_count is None:
             item_count = 1
 
@@ -99,6 +113,9 @@ class RaidController:
 
     @command(command="loot", params=[Const("increase"), Int("item_index")], description="Increase item count", access_level="all")
     def loot_increase_item_cmd(self, request, _, item_index):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         if not self.loot_list:
             return "Loot list is empty."
 
@@ -107,7 +124,7 @@ class RaidController:
 
             if loot_item:
                 loot_item.count = loot_item.count + 1
-                self.last_modify = int(float(time.time()))
+                self.last_modify = int(time.time())
                 return "Increased item count for %s to %d." % (loot_item.item.name, loot_item.count)
             else:
                 return "Item error."
@@ -116,6 +133,9 @@ class RaidController:
 
     @command(command="loot", params=[Const("decrease"), Int("item_index")], description="Decrease item count", access_level="all")
     def loot_decrease_item_cmd(self, request, _, item_index):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         if not self.loot_list:
             return "Loot list is empty."
 
@@ -124,7 +144,7 @@ class RaidController:
 
             if loot_item:
                 loot_item.count = loot_item.count - 1 if loot_item.count > 1 else 1
-                self.last_modify = int(float(time.time()))
+                self.last_modify = int(time.time())
                 return "Decreased item count for %s to %d." % (loot_item.item.name, loot_item.count)
             else:
                 return "Item error."
@@ -148,7 +168,7 @@ class RaidController:
                 name = "You have" if request.channel == "msg" else request.sender.name
                 loot_item.bidders.append(request.sender.name)
 
-                self.last_modify = int(float(time.time()))
+                self.last_modify = int(time.time())
 
                 return "%s moved from %s to %s." % (name, old_item.item.name, loot_item.item.name) if old_item is not None else "%s added to %s." % (name, loot_item.item.name)
             else:
@@ -166,7 +186,7 @@ class RaidController:
                 name = "You were" if request.channel == "msg" else "%s was" % request.sender.name
                 loot_item.bidders.remove(request.sender.name)
 
-                self.last_modify = int(float(time.time()))
+                self.last_modify = int(time.time())
 
                 return "%s removed from %s." % (name, loot_item.item.name)
             else:
@@ -176,6 +196,9 @@ class RaidController:
 
     @command(command="loot", params=[Const("roll")], description="Roll all loot", access_level="all")
     def loot_roll_cmd(self, request, _):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         if self.loot_list:
             blob = ""
 
@@ -204,6 +227,9 @@ class RaidController:
 
     @command(command="loot", params=[Const("reroll")], description="Rebuild loot list", access_level="all")
     def loot_reroll_cmd(self, request, _):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         if self.loot_list:
             count = 1
             for key in sorted(list(self.loot_list.keys())):
@@ -215,7 +241,7 @@ class RaidController:
                     self.loot_list[count] = loot_item
                     count += 1
 
-            self.last_modify = int(float(time.time()))
+            self.last_modify = int(time.time())
 
             return "List has been rebuilt." if len(self.loot_list) > 0 else "No items left to roll."
         else:
@@ -223,6 +249,9 @@ class RaidController:
 
     @command(command="loot", params=[Const("additem"), Const("raid"), Int("item_id"), Int("item_count")], description="Used by the loot lists to add items to loot list", access_level="all")
     def loot_add_raid_item(self, request, _1, _2, item_id, item_count):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         sql = "SELECT * FROM aodb a LEFT JOIN raid_loot r ON (a.name = r.name AND a.highql >= r.ql) WHERE r.id = ? LIMIT 1"
         item = self.db.query_single(sql, [item_id])
 
@@ -235,6 +264,9 @@ class RaidController:
 
     @command(command="loot", params=[Const("addraid"), Const("APF"), Any("category")], description="Add all loot from given raid", access_level="all")
     def loot_add_raid_loot(self, request, _1, _2, category):
+        if not self.leader_controller.can_use_command(request.sender.char_id):
+            return "You're not set as leader, and don't have sufficient access level to override leadership."
+
         items = self.db.query(
             "SELECT r.raid, r.category, r.id, r.ql, r.name, r.comment, r.multiloot, a.lowid, a.highid, a.icon "
             "FROM raid_loot r "
@@ -255,10 +287,12 @@ class RaidController:
 
     @timerevent(budatime="1h", description="Periodically check when loot list was last modified, and clear it if last modification was done 1+ hours ago")
     def loot_clear_event(self, event_type, event_data):
-        if ((int(float(time.time())) - self.last_modify) > 3600) and self.loot_list:
-            self.loot_list = OrderedDict()
-            self.bot.send_org_message("Loot was last modified more than 1 hour ago, list has been cleared.")
-            self.bot.send_private_channel_message("Loot was last modified more than 1 hour ago, list has been cleared.")
+        if self.loot_list and self.last_modify:
+            if int(time.time()) - self.last_modify > 3600 and self.loot_list:
+                self.last_modify = None
+                self.loot_list = OrderedDict()
+                self.bot.send_org_message("Loot was last modified more than 1 hour ago, list has been cleared.")
+                self.bot.send_private_channel_message("Loot was last modified more than 1 hour ago, list has been cleared.")
 
     def is_already_added(self, name):
         for i, loot_item in self.loot_list.items():
@@ -279,4 +313,4 @@ class RaidController:
         }
 
         self.loot_list[end_index] = LootItem(item_ref, item_count)
-        self.last_modify = int(float(time.time()))
+        self.last_modify = int(time.time())
