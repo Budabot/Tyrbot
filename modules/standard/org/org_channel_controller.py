b70dc5237c7903e2c33a1460c06e4cd2d89df6c5
Tyrence <email1@jkbff.com>
2018-09-03 19:27:25 -0600

    notify org channel when characters join and leave the private channel

--- modules/standard/org/org_channel_controller.py
+++ modules/standard/org/org_channel_controller.py
@@ -29,18 +29,41 @@ class OrgChannelController:
             char_name = self.character_service.resolve_char_to_name(event_data.char_id)
             self.bot.send_org_message("[Private] [%s]: %s" % (char_name, event_data.message))
 
+    @event(event_type=PrivateChannelService.JOINED_PRIVATE_CHANNEL_EVENT, description="Notify org channel when a character joins the private channel")
+    def handle_private_channel_joined_event(self, event_type, event_data):
+        char_info = self.pork_service.get_character_info(event_data.char_id)
+        if char_info:
+            name = self.text.format_char_info(char_info)
+        else:
+            char_name = self.character_service.resolve_char_to_name(event_data.char_id)
+            name = "<highlight>%s<end>" % char_name
+
+        self.bot.send_org_message("%s has joined the private channel." % name)
+
+    @event(event_type=PrivateChannelService.LEFT_PRIVATE_CHANNEL_EVENT, description="Notify org channel when a character leaves the private channel")
+    def handle_private_channel_left_event(self, event_type, event_data):
+        char_name = self.character_service.resolve_char_to_name(event_data.char_id)
+        msg = "<highlight>%s<end> has left the private channel." % char_name
+        self.bot.send_org_message(msg)
+
     @event(OrgMemberController.ORG_MEMBER_LOGON_EVENT, "Notify when org member logs on")
     def org_member_logon_event(self, event_type, event_data):
         if self.bot.is_ready():
             char_info = self.pork_service.get_character_info(event_data.char_id)
             if char_info:
-                self.bot.send_org_message("%s has logged on." % self.text.format_char_info(char_info))
+                name = self.text.format_char_info(char_info)
             else:
                 char_name = self.character_service.resolve_char_to_name(event_data.char_id)
-                self.bot.send_org_message("<highlight>%s<end> has logged on." % char_name)
+                name = "<highlight>%s<end>" % char_name
+
+            msg = "%s has logged on." % name
+            self.bot.send_org_message(msg)
+            self.bot.send_private_channel_message(msg)
 
     @event(OrgMemberController.ORG_MEMBER_LOGOFF_EVENT, "Notify when org member logs off")
     def org_member_logoff_event(self, event_type, event_data):
         if self.bot.is_ready():
             char_name = self.character_service.resolve_char_to_name(event_data.char_id)
-            self.bot.send_org_message("<highlight>%s<end> has logged off." % char_name)
+            msg = "<highlight>%s<end> has logged off." % char_name
+            self.bot.send_org_message(msg)
+            self.bot.send_private_channel_message(msg)
