1e6e8cb44ad75de40375b8b21bc803399da97d06
Tyrence <email1@jkbff.com>
2018-09-04 03:40:53 -0600

    added !pocketboss

--- modules/standard/pocketboss/pocketboss_controller.py
+++ modules/standard/pocketboss/pocketboss_controller.py
@@ -1,6 +1,6 @@
-from core.decorators import instance, command, event
-from core.command_param_types import Any, Character
-from core.private_channel_service import PrivateChannelService
+from core.chat_blob import ChatBlob
+from core.decorators import instance, command
+from core.command_param_types import Any
 
 
 @instance()
@@ -9,14 +9,38 @@ class PocketbossController:
         pass
 
     def inject(self, registry):
-        self.bot = registry.get_instance("bot")
         self.db = registry.get_instance("db")
-        self.private_channel_service = registry.get_instance("private_channel_service")
-        self.character_service = registry.get_instance("character_service")
-        self.job_scheduler = registry.get_instance("job_scheduler")
-        self.access_service = registry.get_instance("access_service")
+        self.text = registry.get_instance("text")
 
-    @command(command="pocketboss", params=[], access_level="all",
-             description="Join the private channel")
-    def pocketboss_cmd(self, request):
-        self.private_channel_service.invite(request.sender.char_id)
+    @command(command="pocketboss", params=[Any("search")], access_level="all",
+             description="Show information about a pocketboss")
+    def pocketboss_cmd(self, request, search):
+        data = self.search_for_pocketboss(search)
+
+        num = len(data)
+        if num == 1:
+            row = data[0]
+            blob = "Location: <highlight>%s, %s<end>\n" % (row.long_name, row.location)
+            blob += "Found on: <highlight>%s, Level %d<end>\n\n" % (row.mob_type, row.level)
+            symbs = self.db.query("SELECT a.* FROM pocketboss_loot p "
+                                  "LEFT JOIN aodb a ON p.item_id = a.highid WHERE pocketboss_id = ? "
+                                  "ORDER BY a.highql DESC, a.name ASC", [row.id])
+            for symb in symbs:
+                blob += "%s (%d)\n" % (self.text.make_item(symb.lowid, symb.highid, symb.highql, symb.name), symb.highql)
+
+            return ChatBlob("Remains of %s" % row.name, blob)
+
+        else:
+            blob = ""
+            for row in data:
+                blob += self.text.make_chatcmd(row.name, "/tell <myname> pocketboss %s" % row.name) + "\n"
+
+            return ChatBlob("Pocketboss Search Results (%d)" % num, blob)
+
+    def search_for_pocketboss(self, search):
+        row = self.db.query_single("SELECT p1.*, p2.long_name FROM pocketboss p1 LEFT JOIN playfields p2 ON p1.playfield_id = p2.id WHERE name LIKE ?", [search])
+        if row:
+            return [row]
+
+        return self.db.query(*self.db.handle_extended_like("SELECT p1.*, p2.long_name FROM pocketboss p1 LEFT JOIN playfields p2 ON p1.playfield_id = p2.id "
+                                                           "WHERE name <EXTENDED_LIKE=0> ? ORDER BY name ASC", [search]))
