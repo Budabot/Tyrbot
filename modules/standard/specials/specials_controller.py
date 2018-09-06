e96d7f80cb02fa4537094d016320c9e49bff7119
Tyrence <email1@jkbff.com>
2018-09-06 03:39:11 -0600

    added !brawl

--- modules/standard/specials/specials_controller.py
+++ modules/standard/specials/specials_controller.py
@@ -2,10 +2,7 @@ import math
 
 from core.chat_blob import ChatBlob
 from core.decorators import instance, command
-from core.command_param_types import Any, Int, Decimal
-import os
-import re
-import json
+from core.command_param_types import Int, Decimal
 
 from core.dict_object import DictObject
 
@@ -18,6 +15,7 @@ class SpecialsController:
     def inject(self, registry):
         self.db = registry.get_instance("db")
         self.text = registry.get_instance("text")
+        self.util = registry.get_instance("util")
         self.items_controller = registry.get_instance("items_controller")
         self.command_alias_service = registry.get_instance("command_alias_service")
 
@@ -44,7 +42,7 @@ class SpecialsController:
         blob += "Init needed for max speed at Full Agg (100%%): <highlight>%d<end>\n" % inits_full_agg
         blob += "Init needed for max speed at Neutral (88%%): <highlight>%d<end>\n" % inits_neutral
         blob += "Init needed for max speed at Full Def (0%%): <highlight>%d<end>\n\n" % inits_full_def
-        blob += "Note that at the neutral position (88%), your attack and recharge time will match that of the weapon you are using.\n\n"
+        blob += "Note that at the neutral position (88%), your attack and recharge time will match that of the weapon you are using.\n\n\n"
         blob += "Based on the !aggdef command from Budabot, which was based upon a RINGBOT module made by NoGoal(RK2) and modified for Budabot by Healnjoo(RK2)"
 
         return ChatBlob("Agg/Def Results", blob)
@@ -62,6 +60,22 @@ class SpecialsController:
         blob += "You will need <highlight>%d<end> Aimed Shot Skill to cap your recharge at <highlight>%d<end> seconds." % (as_info.skill_cap, as_info.hard_cap)
 
         return ChatBlob("Aimed Shot Results", blob)
+
+    @command(command="brawl", params=[Int("brawl_skill")], access_level="all",
+             description="Show brawl information for a particular amout of brawl skill")
+    def brawl_cmd(self, request, brawl_skill):
+        brawl_info = self.get_brawl_info(brawl_skill)
+
+        blob = "Brawl Skill: <highlight>%d<end>\n\n" % brawl_skill
+        blob += "Brawl Recharge: <highlight>15<end> seconds (constant)\n"
+        blob += "Brawl Damage: <highlight> %d - %d (%d)<end>\n" % (brawl_info.min_dmg, brawl_info.max_dmg, brawl_info.crit_dmg)
+        blob += "Stun Change: <highlight>%d%%<end>\n" % brawl_info.stun_chance
+        blob += "Stun Duration: <highlight>%d<end> seconds\n\n" % brawl_info.stun_duration
+        blob += "Stun chance is 10% for brawl skill less than 1000 and 20% for brawl skill 1000 or greater.\n"
+        blob += "Stun duration is 3 seconds for brawl skill less than 2001 and 4 seconds for brawl skill 2001 or greater.\n\n\n"
+        blob += "Based on the !brawl command from Budabot by Imoutochan (RK1)"
+
+        return ChatBlob("Brawl Results", blob)
 
     def get_init_result(self, weapon_attack, weapon_recharge, init_skill):
         if init_skill < 1200:
@@ -109,3 +123,17 @@ class SpecialsController:
         result.recharge = as_recharge
 
         return result
+
+    def get_brawl_info(self, brawl_skill):
+        min_values = {1: 1, 1000: 100, 1001: 101, 2000: 170, 2001: 171, 3000: 235}
+        max_values = {1: 2, 1000: 500, 1001: 501, 2000: 850, 2001: 851, 3000: 1145}
+        crit_values = {1: 3, 1000: 500, 1001: 501, 2000: 600, 2001: 601, 3000: 725}
+
+        brawl_info = DictObject()
+        brawl_info.min_dmg = self.util.interpolate_value(brawl_skill, min_values)
+        brawl_info.max_dmg = self.util.interpolate_value(brawl_skill, max_values)
+        brawl_info.crit_dmg = self.util.interpolate_value(brawl_skill, crit_values)
+        brawl_info.stun_chance = 10 if brawl_skill < 1000 else 20
+        brawl_info.stun_duration = 3 if brawl_skill < 2001 else 4
+
+        return brawl_info
