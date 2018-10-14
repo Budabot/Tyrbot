32b24baa21da9a6c983d7a6d80ba76b6058e776f
Tyrence <email1@jkbff.com>
2018-10-14 16:25:45 -0600

    allow multiple levels of aliases

--- core/command_service.py
+++ core/command_service.py
@@ -142,8 +142,10 @@ class CommandService:
             # check for command alias
             command_alias = self.command_alias_service.check_for_alias(command_str)
 
-            if command_alias:
+            while command_alias:
                 command_str, command_args = self.get_command_parts(command_alias + command_args)
+
+                command_alias = self.command_alias_service.check_for_alias(command_str)
 
             cmd_configs = self.get_command_configs(command_str, channel, 1)
             if cmd_configs:
