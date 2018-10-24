7136ddcfbd7d006348bedce076fdce6538ff687f
Tyrence <email1@jkbff.com>
2018-10-23 21:53:02 -0600

    remove debug code

--- modules/standard/whereis/whereis_controller.py
+++ modules/standard/whereis/whereis_controller.py
@@ -27,7 +27,7 @@ class WhereisController:
                     blob += " " + self.text.make_chatcmd("waypoint: %sx%s %s" % (row.xcoord, row.ycoord, row.short_name),
                                                          "/waypoint %s %s %d" % (row.xcoord, row.ycoord, row.playfield_id))
                 blob += "\n\n"
-            return ChatBlob("Whereis '%s' (%d)" % (search, count), blob, "this is a footer")
+            return ChatBlob("Whereis '%s' (%d)" % (search, count), blob)
         else:
             return "Could not find any results for your search."
 
