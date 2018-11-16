695db33a7a1496fba505b3b54337c735dac35014
Tyrence <email1@jkbff.com>
2018-11-15 23:01:38 -0700

    renamed !loud to !shout

--- modules/standard/raid/chat_controller.py
+++ modules/standard/raid/chat_controller.py
@@ -1,20 +1,16 @@
 from core.decorators import instance, command
 from core.command_param_types import Any
-from core.db import DB
-from core.text import Text
 
 
 @instance()
 class ChatController:
     def inject(self, registry):
-        self.db: DB = registry.get_instance("db")
-        self.text: Text = registry.get_instance("text")
         self.command_alias_service = registry.get_instance("command_alias_service")
 
     def start(self):
-        self.command_alias_service.add_alias("cmd", "loud")
+        self.command_alias_service.add_alias("cmd", "shout")
 
-    @command(command="loud", params=[Any("message")], access_level="all",
+    @command(command="shout", params=[Any("message")], access_level="all",
              description="Show a highly visible message", extended_description="This command is similar to <symbol>cmd in Budabot")
-    def loud_command(self, request, message):
+    def shout_command(self, request, message):
         return "\n<yellow>---------------------\n<red>%s<end>\n<yellow>---------------------" % message
