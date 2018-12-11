418d5fd77fd6781903b75bc899292fcb2005f3ca
Tyrence <email1@jkbff.com>
2018-12-10 22:10:40 -0700

    !lastseen now checks if bot is in an org

--- modules/standard/org/last_seen_controller.py
+++ modules/standard/org/last_seen_controller.py
@@ -13,10 +13,14 @@ class LastSeenController:
         self.db = registry.get_instance("db")
         self.util = registry.get_instance("util")
         self.character_service = registry.get_instance("character_service")
+        self.public_channel_service = registry.get_instance("public_channel_service")
 
     @command(command="lastseen", params=[Character("character")], access_level="admin",
              description="Show the last time an org member was online (on any alt)")
     def lastseen_cmd(self, request, char):
+        if not self.public_channel_service.get_org_id:
+            return "Error! Bot must be in an org in order to use this command."
+
         sql = """
             SELECT
                 p.*,
