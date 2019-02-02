f4cdb0ce6228d7eb81b0b9bcdf46954c86eccac2
Tyrence <email1@jkbff.com>
2019-02-01 19:19:12 -0700

    !runas requires sufficient access level to run commands as another character

--- modules/core/system/runas_controller.py
+++ modules/core/system/runas_controller.py
@@ -8,6 +8,7 @@ class RunasController:
         self.bot = registry.get_instance("bot")
         self.command_service = registry.get_instance("command_service")
         self.setting_service = registry.get_instance("setting_service")
+        self.access_service = registry.get_instance("access_service")
 
     @command(command="runas", params=[Character("character"), Any("command")], access_level="superadmin",
              description="Run a command as another character")
@@ -17,5 +18,7 @@ class RunasController:
 
         if not char.char_id:
             return "Could not find character <highlight>%s<end>" % char.name
+        elif not self.access_service.has_sufficient_access_level(request.sender.char_id, char.char_id):
+            return "Error! You must have a higher access level than <highlight>%s<end>." % char.name
         else:
             self.command_service.process_command(command_str, request.channel, char.char_id, request.reply)
