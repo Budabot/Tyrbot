36bc2dd04bc9ac1647d4d6521e56af5b007c0d30
Tyrence <email1@jkbff.com>
2019-03-22 01:18:42 -0600

    code cleanup for !leader

--- modules/standard/raid/leader_controller.py
+++ modules/standard/raid/leader_controller.py
@@ -34,33 +34,12 @@ class LeaderController:
     @command(command="leader", params=[Const("clear")], access_level="all",
              description="Clear the current raid leader")
     def leader_clear_command(self, request, _):
-        if not self.leader:
-            return "There is no current raid leader."
-        elif self.leader.char_id == request.sender.char_id:
-            self.leader = None
-            return "You have been removed as raid leader."
-        elif self.access_service.has_sufficient_access_level(request.sender.char_id, self.leader.char_id):
-            old_leader = self.leader
-            self.leader = None
-            return "You have removed <highlight>%s<end> as raid leader." % old_leader.name
-        else:
-            return "You do not have a high enough access level to remove raid leader from <highlight>%s<end>." % self.leader.name
+        return self.set_raid_leader(request.sender, None)
 
     @command(command="leader", params=[Const("set")], access_level="all",
              description="Set (or unset) yourself as raid leader")
     def leader_set_self_command(self, request, _):
-        if not self.leader:
-            self.leader = request.sender
-            return "You have been set as raid leader."
-        elif self.leader.char_id == request.sender.char_id:
-            self.leader = None
-            return "You have been removed as raid leader."
-        elif self.access_service.has_sufficient_access_level(request.sender.char_id, self.leader.char_id):
-            old_leader = self.leader
-            self.leader = request.sender
-            return "You have taken raid leader from <highlight>%s<end>." % old_leader.name
-        else:
-            return "You do not have a high enough access level to take raid leader from <highlight>%s<end>." % self.leader.name
+        return self.set_raid_leader(request.sender, request.sender)
 
     @command(command="leader", params=[Const("set", is_optional=True), Character("character")], access_level="all",
              description="Set another character as raid leader")
@@ -68,11 +47,7 @@ class LeaderController:
         if not char.char_id:
             return "Could not find <highlight>%s<end>." % char.name
 
-        if not self.leader or self.access_service.has_sufficient_access_level(request.sender.char_id, self.leader.char_id):
-            self.leader = char
-            return "<highlight>%s<end> has been set as raid leader." % char.name
-        else:
-            return "You do not have a high enough access level to take raid leader from <highlight>%s<end>." % self.leader.name
+        return self.set_raid_leader(request.sender, char)
 
     @timerevent(budatime="1h", description="Remove raid leader if raid leader hasn't been active for more than 1 hour")
     def leader_auto_remove(self, event_type, event_data):
@@ -81,8 +56,8 @@ class LeaderController:
                 self.leader = None
                 self.last_activity = None
 
-                self.bot.send_private_channel_message("Leader has been automatically cleared because of inactivity.")
-                self.bot.send_org_message("Leader has been automatically cleared because of inactivity.")
+                self.bot.send_private_channel_message("Raid leader has been automatically cleared because of inactivity.")
+                self.bot.send_org_message("Raid leader has been automatically cleared because of inactivity.")
 
     @event(PrivateChannelService.LEFT_PRIVATE_CHANNEL_EVENT, "Remove raid leader if raid leader leaves private channel")
     def leader_remove_on_leave_private(self, event_type, event_data):
@@ -107,3 +82,39 @@ class LeaderController:
             return True
 
         return False
+
+    def set_raid_leader(self, sender, settee):
+        if settee is None:
+            if not self.leader:
+                return "There is no current raid leader."
+            elif self.leader.char_id == sender.char_id:
+                self.leader = None
+                return "You have been removed as raid leader."
+            elif self.access_service.has_sufficient_access_level(sender.char_id, self.leader.char_id):
+                old_leader = self.leader
+                self.leader = None
+                self.bot.send_private_message(old_leader.char_id, "You have been removed as raid leader by <highlight>%s<end>." % sender.name)
+                return "You have removed <highlight>%s<end> as raid leader." % old_leader.name
+            else:
+                return "You do not have a high enough access level to remove raid leader from <highlight>%s<end>." % self.leader.name
+        elif sender.char_id == settee.char_id:
+            if not self.leader:
+                self.leader = sender
+                return "You have been set as raid leader."
+            elif self.leader.char_id == sender.char_id:
+                self.leader = None
+                return "You have been removed as raid leader."
+            elif self.access_service.has_sufficient_access_level(sender.char_id, self.leader.char_id):
+                old_leader = self.leader
+                self.leader = sender
+                self.bot.send_private_message(old_leader.char_id, "<highlight>%s<end> has taken raid leader from you." % sender.name)
+                return "You have taken raid leader from <highlight>%s<end>." % old_leader.name
+            else:
+                return "You do not have a high enough access level to take raid leader from <highlight>%s<end>." % self.leader.name
+        else:
+            if not self.leader or self.access_service.has_sufficient_access_level(sender.char_id, self.leader.char_id):
+                self.leader = settee
+                self.bot.send_private_message(settee.char_id, "<highlight>%s<end> has set you as raid leader." % sender.name)
+                return "<highlight>%s<end> has been set as raid leader." % settee.name
+            else:
+                return "You do not have a high enough access level to take raid leader from <highlight>%s<end>." % self.leader.name
