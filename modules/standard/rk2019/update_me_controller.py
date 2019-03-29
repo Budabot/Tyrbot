ce03a554f00dc0d2d5b455e656d262e8c1484a5d
Tyrence <email1@jkbff.com>
2019-03-28 23:29:48 -0600

    fix !updateme for omni

--- modules/standard/rk2019/update_me_controller.py
+++ modules/standard/rk2019/update_me_controller.py
@@ -4,19 +4,23 @@ from core.command_param_types import Any
 from core.decorators import instance, command
 import re
 
+from core.dict_object import DictObject
+
 
 @instance()
 class UpdateMeController:
     def __init__(self):
-        self.regexes = {"breed": re.compile(r"Breed: (.+)"),
-                        "level": re.compile(r"Level: (\d+)"),
-                        "gender": re.compile(r"Gender: (.+)"),
-                        "faction": re.compile(r"Alignment: (.+)"),
-                        "profession": re.compile(r"Profession: (.+)", re.MULTILINE),
-                        "profession_title": re.compile(r"Profession title: (.+) \(TitleLevel \d\)"),
-                        "ai_rank": re.compile(r"Defender rank: (.+)"),
-                        "org_name": re.compile(r"Clan: (.+)"),
-                        "org_rank_name": re.compile(r"Clan title:(.+)")}
+        self.regexes = [DictObject({"name": "breed", "regex": re.compile(r"Breed: (.+)")}),
+                        DictObject({"name": "level", "regex": re.compile(r"Level: (\d+)")}),
+                        DictObject({"name": "gender", "regex": re.compile(r"Gender: (.+)")}),
+                        DictObject({"name": "faction", "regex": re.compile(r"Alignment: (.+)")}),
+                        DictObject({"name": "profession", "regex": re.compile(r"Profession: (.+)", re.MULTILINE)}),
+                        DictObject({"name": "profession_title", "regex": re.compile(r"Profession title: (.+) \(TitleLevel \d\)")}),
+                        DictObject({"name": "ai_rank", "regex": re.compile(r"Defender rank: (.+)")}),
+                        DictObject({"name": "org_name", "regex": re.compile(r"Clan: (.+)")}),
+                        DictObject({"name": "org_name", "regex": re.compile(r"Detachment: (.+)")}),
+                        DictObject({"name": "org_rank_name", "regex": re.compile(r"Detachment title:(.+)")}),
+                        DictObject({"name": "org_rank_name", "regex": re.compile(r"Clan title:(.+)")})]
 
     def inject(self, registry):
         self.bot = registry.get_instance("bot")
@@ -28,16 +32,24 @@ class UpdateMeController:
         if self.bot.dimension == 5:
             return "This command cannot be used on this server."
 
+        newline = re.compile(r"\n")
+        result = newline.search(char_info)
+        if not result:
+            return "This command must be used from a tell window (click the bot name in chat to open a tell window)."
+
         char_info_obj = self.pork_service.get_from_database(char_id=request.sender.char_id)
         char_info_obj.name = request.sender.name
         char_info_obj.dimension = self.bot.dimension
         char_info_obj.source = "updateme"
         char_info_obj.last_updated = int(time.time())
 
-        for key, regex in self.regexes.items():
-            result = regex.search(char_info)
+        for item in self.regexes:
+            result = item.regex.search(char_info)
             if result:
-                char_info_obj[key] = result.group(1)
+                char_info_obj[item.name] = result.group(1)
+
+        if char_info_obj.faction:
+            char_info_obj.faction = char_info_obj.faction.capitalize()
 
         self.pork_service.save_character_info(char_info_obj)
 
