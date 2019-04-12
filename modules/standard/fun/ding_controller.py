41a681c14c8dcae3ba0a9c879dfbd461a7908481
Roovers,Jeroen J <jroovers93@gmaill.com>
2019-04-12 23:04:11 +0200

    Fun module port

---
+++ modules/standard/fun/ding_controller.py
@@ -0,0 +1,137 @@
+import random
+
+from core.command_param_types import Any
+from core.decorators import command, instance
+from core.registry import Registry
+
+
+@instance()
+class DingController:
+    """
+        Port of Budabot fun module
+    """
+
+    def inject(self, registry: Registry):
+        self.command_alias_service = registry.get_instance(
+            "command_alias_service")
+
+    def start(self):
+        self.command_alias_service.add_alias("levelup", "ding")
+
+    @command(command="ding", params=[Any("level", is_optional=True)], access_level="all",
+             description="Show a random ding message, congratz!")
+    def ding_command(self, request, level: str):
+        if level is None:
+            text_options = [
+                "Yeah yeah gratz, I would give you a better response but you didn't say what level you dinged.",
+                "Hmmm, I really want to know what level you dinged, but gratz anyways nub.",
+                "When are you people going to start using me right! Gratz for your level though.",
+                "Gratz! But what are we looking at? I need a level next time."]
+            return random.choice(text_options)
+        else:
+            try:
+                current_level = int(float(level))
+                levels_to_go = 220 - current_level
+                character_name = request.sender.name
+
+                text_options = [
+                    ("Something unexpected happened. Try again later.")]
+                if current_level <= 0:
+                    text_options.clear()
+                    text_options = [("Reclaim sure is doing a number on you if you're going backwards..."),
+                                    ("Wtb negative exp kite teams!"),
+                                    ("That leaves you with... %d more levels until 220, I don't see the problem?" % (
+                                        levels_to_go)),
+                                    ("How the hell did you get to %d?" % (current_level))]
+                elif current_level == 1:
+                    text_options.clear()
+                    text_options = [("You didn't even start yet..."),
+                                    ("Did you somehow start from level 0?"),
+                                    ("Dinged from 1 to 1? Congratz")]
+                elif current_level == 15:
+                    text_options.clear()
+                    text_options = [("You're <yellow>TL 2<end> already? Nice."),
+                                    ("Title level 2, Congratulations! Go spend your well earned IP!")]
+                elif current_level == 25:
+                    text_options.clear()
+                    text_options = [("Congratulations! Make sure to finish up in subway before moving on!"),
+                                    ("<red>*Abmouth Supremus sweating profusely*<end> Congratz on your 25th, %s!") % (
+                                        character_name)]
+                elif current_level == 50:
+                    text_options.clear()
+                    text_options = [("Yay, <yellow>TL 3<end> Congratulations, %s!" % (character_name)),
+                                    ("Title level 3, Congratulations! Go spend your well earned IP!")]
+                elif current_level == 60:
+                    text_options.clear()
+                    text_options = [("Congratulations! Make sure to finish up in ToTW before moving on!"),
+                                    ("Nice! Time to test out Aztur's immortality? Congratz on your 60th level, %s!") % (
+                                        character_name)]
+                elif current_level == 100:
+                    text_options.clear()
+                    text_options = [("Congratz! <red>Level 100<end> - %s you rock!" % (character_name)),
+                                    ("Congratulations! Time to twink up for T.I.M!"),
+                                    ("Gratz, you're half way to 200. More missions, MORE!"),
+                                    ("Woot! Congrats, don't forget to put on your 1k token board.")]
+                elif current_level == 150:
+                    # foo
+                    text_options.clear()
+                    text_options = [("S10 time!!!"),
+                                    ("Time to ungimp yourself! Horray!. Congrats =)"),
+                                    ("What starts with A, and ends with Z? <green>ALIUMZ!<end>"),
+                                    ("Wow, is it that time already? TL 5 really? You sure are moving along! Gratz")]
+                elif current_level == 180:
+                    # foo
+                    text_options.clear()
+                    text_options = [("Congratz! Now go kill some <green>aliumz<end> at S13/28/35!!"),
+                                    ("Only 20 more froob levels to go! HOORAH!"),
+                                    ("Yay, only 10 more levels until TL 6! Way to go!")]
+                elif current_level == 190:
+                    # foo
+                    text_options.clear()
+                    text_options = [("Wow holy shiznits! You're TL 6 already? Congrats!"),
+                                    ("Just a few more steps and you're there buddy, keep it up!"),
+                                    (
+                                            "Almost party time! just a bit more to go %s. We'll be sure to bring you a cookie!" % (
+                                        character_name))]
+                elif current_level == 200:
+                    # foo
+                    text_options.clear()
+                    text_options = [("Congratz! The big Two Zero Zero!!! Party at %s's place" % (character_name)),
+                                    ("Best of the best in froob terms, congratulations!"),
+                                    ("What a day indeed. Finally done with froob levels. Way to go!")]
+                elif current_level > 200 and current_level < 220:
+                    # foo
+                    text_options.clear()
+                    text_options = [("Congratz! Just a few more levels to go!"),
+                                    ("Enough with the dingin you are making the fr00bs feel bad!"),
+                                    ("Come on save some dings for the rest!")]
+                elif current_level == 220:
+                    # foo
+                    text_options.clear()
+                    text_options = [("Congratz! You have reached the end of the line! NO MORE FUN FOR YOU! :P"),
+                                    ("Holy shit, you finally made it! What an accomplishment... Congratulations %s, for reaching a level reserved for the greatest!" % (
+                                        character_name)),
+                                    ("I'm going to miss you a great deal, because after this, we no longer can be together %s. We must part so you can continue getting your research and AI levels done! Farewell!" % (
+                                        character_name)),
+                                    ("How was the inferno grind? I'm glad to see you made it through, and congratulations for finally getting the level you well deserved!"),
+                                    ("Our congratulations, to our newest level 220 member, %s, for his dedication. We present him with his new honorary rank, Chuck Norris!" % (
+                                        character_name)),
+                                    ("Holy crap, you actually did it! Dear %s, I salute you for your determination and sheer awesomeness. Congratulations, to our newest level 220 member!" % (
+                                        character_name))]
+                elif current_level > 220:
+                    text_options.clear()
+                    text_options = [("Umm...no."),
+                                    ("You must be high, because that number is too high..."),
+                                    ("Ha, ha, ha..."),
+                                    ("You must be a GM or one hell of an exploiter, that number it too high!"),
+                                    ("Yeah, and I'm Chuck Norris..."),
+                                    ("Not now, not later, not ever... find a more reasonable level!")]
+                else:
+                    text_options.clear()
+                    text_options = [("Ding ding ding... now ding some more!"),
+                                    ("Keep em coming!"),
+                                    ("Don't stop now, you're getting there!"),
+                                    ("Come on, COME ON! Only %d more levels to go until 220!") % (levels_to_go)]
+                return random.choice(text_options)
+            except (ValueError):
+                return ("I can only process single numbers with this command")
---
+++ modules/standard/fun/fun_controller.py
@@ -0,0 +1,78 @@
+from core.command_param_types import Int
+from core.db import DB
+from core.decorators import command, instance
+import random
+
+
+@instance()
+class FunController:
+    """
+        Port of Budabot fun module
+    """
+
+    def inject(self, registry):
+        self.db: DB = registry.get_instance("db")
+        self.command_alias_service = registry.get_instance("command_alias_service")
+
+    def start(self):
+        self.command_alias_service.add_alias("pinky", "brain")
+        self.command_alias_service.add_alias("norris", "chuck")
+        self.command_alias_service.add_alias("chucknorris", "chuck")
+        self.command_alias_service.add_alias("cyb0r", "cybor")
+        self.command_alias_service.add_alias("cyber", "cybor")
+        self.command_alias_service.add_alias("office", "dwight")
+        self.command_alias_service.add_alias("funcom", "fc")
+        self.command_alias_service.add_alias("simpsons", "homer")
+
+    @command(command="beer", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random beer message")
+    def beer_command(self, request, item_id):
+        return self.get_fun_message("beer", item_id)
+
+    @command(command="brain", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random pinky and the brain quote")
+    def brain_command(self, request, item_id):
+        return self.get_fun_message("brain", item_id)
+
+    @command(command="chuck", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random Chuck Norris joke")
+    def chuck_command(self, request, item_id):
+        return self.get_fun_message("chuck", item_id)
+
+    @command(command="cybor", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random cybor message Caution: HOT!")
+    def cybor_command(self, request, item_id):
+        return self.get_fun_message("cybor", item_id)
+
+    @command(command="dwight", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random Dwight quote")
+    def dwight_command(self, request, item_id):
+        return self.get_fun_message("dwight", item_id)
+
+    @command(command="fc", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random FC quote")
+    def fc_command(self, request, item_id):
+        return self.get_fun_message("fc", item_id)
+
+    @command(command="homer", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random homer quote")
+    def homer_command(self, request, item_id):
+        return self.get_fun_message("homer", item_id)
+
+    @command(command="pirates", params=[Int("item_id", is_optional=True)],
+             access_level="member", description="Shows a random Pirates of the Caribbean quote")
+    def pirates_command(self, request, item_id):
+        return self.get_fun_message("pirates", item_id)
+
+    def get_fun_message(self, quote_type, number):
+        if number is None:
+            number = -1
+        data = self.db.query("SELECT f.content FROM fun f WHERE type = ?", [str(quote_type)])
+        if number == -1:
+            row = random.choice(data)
+        else:
+            try:
+                row = data[number]
+            except IndexError:
+                return "There is no item with that id."
+        return row.content;
