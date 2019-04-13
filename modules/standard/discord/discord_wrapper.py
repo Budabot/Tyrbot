b4161b6fe8dafd5762381eecfe61c026d327410a
Adrian Johnson <aj@dustify.com>
2019-04-13 05:02:31 +0100

    discord online members formatting

--- modules/standard/discord/discord_wrapper.py
+++ modules/standard/discord/discord_wrapper.py
@@ -32,6 +32,74 @@ class DiscordWrapper(discord.Client):
             if cid in self.channels:
                 if self.channels[cid].relay_dc:
                     self.dqueue.append(("discord_message", message))
+
+    # discord custom text translations
+    def translate_content(self, content):
+        # online members table, only use if any chars online
+        if content.title == "Online" and content.description != "\nNo characters online.":
+            # split content into rows
+            rowSplit = content.description.split("\n")
+
+            maxNameLength = 0
+            maxLevelLength = 0
+            maxSideLength = 0
+            maxProfLength = 0
+
+            onlineMembers = []
+            
+            # iterate rows to calc table size
+            for i in range(len(rowSplit)):
+                # get column split
+                columnSplit = rowSplit[i].split(" ")
+
+                # if we don't have 4 items then skip
+                if len(columnSplit) != 4:
+                    continue
+
+                # store split for use later
+                onlineMembers.append(columnSplit)
+
+                # get string lengths
+                nameLength = len(columnSplit[0])
+                levelLength = len(columnSplit[1])
+                sideLength = len(columnSplit[2])
+                profLength = len(columnSplit[3])
+
+                # compare and apply lengths as required
+                if nameLength > maxNameLength:
+                    maxNameLength = nameLength
+
+                if levelLength > maxLevelLength:
+                    maxLevelLength = levelLength
+
+                if sideLength > maxSideLength:
+                    maxSideLength = sideLength
+
+                if profLength > maxProfLength:
+                    maxProfLength = profLength
+                   
+            # add an additional space
+            maxNameLength += 1
+            maxLevelLength += 1
+            maxSideLength += 1
+            maxProfLength += 1
+
+            # start monospace
+            content.description = "```"
+
+            # iterate rows to build table
+            for i in range(len(onlineMembers)):
+                # get online member
+                onlineMember = onlineMembers[i]
+                content.description += onlineMember[0].ljust(maxNameLength);
+                content.description += onlineMember[1].ljust(maxLevelLength);
+                content.description += onlineMember[2].ljust(maxSideLength);
+                content.description += onlineMember[3].ljust(maxProfLength);
+                content.description += "\n"                
+
+            # end monospace
+            content.description += "```"
+
     
     async def relay_message(self):
         await self.wait_until_ready()
@@ -54,6 +122,9 @@ class DiscordWrapper(discord.Client):
                         for cid, channel in self.channels.items():
                             if channel.relay_ao:
                                 if message.get_type() == "embed":
+                                    # do custom text translations
+                                    self.translate_content(content)
+
                                     await self.send_message(discord.Object(id=cid), embed=content)
                                 else:
                                     await self.send_message(discord.Object(id=cid), content)
