6207655c2cc254ae85aa4e77a82625a7471040ed
Tyrence <email1@jkbff.com>
2019-06-08 01:35:13 -0600

    removed docker_host export

--- dev.sh
+++ dev.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-export DOCKER_HOST=$(route -n | awk '/UG[ \t]/{print $2}')
-echo "$(route -n | awk '/UG[ \t]/{print $2}')      docker_host" >> /etc/hosts
-
 set -o pipefail -o errexit
 
 # The bot uses non-zero exit codes to signal state.
