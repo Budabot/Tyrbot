e88f1ea7307d82abace32bffe092ea3a0db38e0c
Einar Forselv <eforselv@gmail.com>
2019-06-20 20:43:52 +0200

    Automatically create virtualenv in start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-pip3 install -U -r requirements.txt
+# Ensure virtualenv is present. This is not always the case
+python3 -m pip install virtualenv --user
+
+# Create and activate the virtualenv. This can be done even if it already exists
+# and will ensure setuptools, wheel and pip are up to date
+python3 -m virtualenv venv
+source venv/bin/activate
+
+# From there on we use 'pip' and 'python' (refers to versions in the virtualenv)
+pip install -U -r requirements.txt
 
 set -o pipefail -o errexit
 
 # The bot uses non-zero exit codes to signal state.
 # The bot will restart until it returns an exit code of zero.
 while true; do
-    python3 bootstrap.py && exit
+    python bootstrap.py && exit
     sleep 1
 done
