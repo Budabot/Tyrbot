17bb917fde52e53436c069e6bd2b1ada88dd1968
deathlef <48448008+deathlef@users.noreply.github.com>
2020-12-16 00:19:28 +0100

    adjusted ORDER by highid
    "ORDER BY highid = ?" is a trick to get the item with that specific highid as the first entry

--- modules/standard/items/items_controller.py
+++ modules/standard/items/items_controller.py
@@ -136,13 +136,13 @@ class ItemsController:
 
     def get_by_item_id(self, item_id, ql=None):
         if ql:
-            return self.db.query_single("SELECT * FROM aodb WHERE (highid = ? OR lowid = ?) AND (lowql <= ? AND highql >= ?) ORDER BY highid DESC LIMIT 1", [item_id, item_id, ql, ql])
+            return self.db.query_single("SELECT * FROM aodb WHERE (highid = ? OR lowid = ?) AND (lowql <= ? AND highql >= ?) ORDER BY highid = ? LIMIT 1", [item_id, item_id, ql, ql, item_id])
         else:
-            return self.db.query_single("SELECT * FROM aodb WHERE highid = ? OR lowid = ? ORDER BY highid DESC LIMIT 1", [item_id, item_id])
+            return self.db.query_single("SELECT * FROM aodb WHERE highid = ? OR lowid = ? ORDER BY highid = ? LIMIT 1", [item_id, item_id])
 
     def find_by_name(self, name, ql=None):
         if ql:
-            return self.db.query_single("SELECT * FROM aodb WHERE name = ? AND lowql <= ? AND highql >= ? ORDER BY highid DESC LIMIT 1", [name, ql, ql])
+            return self.db.query_single("SELECT * FROM aodb WHERE name = ? AND lowql <= ? AND highql >= ? ORDER BY highid DESC LIMIT 1", [name, ql, ql, item_id])
         else:
             return self.db.query_single("SELECT * FROM aodb WHERE name = ? ORDER BY highql DESC, highid DESC LIMIT 1", [name])
 
