59b7974c120ab396347065db7777298e4ba8e977
Tyrence <email1@jkbff.com>
2021-01-03 01:11:10 -0700

    wait 10 seconds before restarting in windows if there was an error

--- start.bat
+++ start.bat
@@ -14,7 +14,13 @@ REM The bot uses non-zero exit codes to signal state.
 REM The bot will restart until it returns an exit code of zero.
 if %errorlevel% == 0 goto end
 
-timeout /t 1
+if %errorlevel% == 2 goto restart
+
+timeout /t 10
+goto :install
+
+:restart
+timeout /t 5
 goto :install
 
 :end
