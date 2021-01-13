a7f75f2be32cabc35dfc217b65f08f73da629bfd
Tyrence <email1@jkbff.com>
2021-01-13 09:41:58 -0700

    added __str__ and __eq__ to ChatBlob

--- core/chat_blob.py
+++ core/chat_blob.py
@@ -4,3 +4,16 @@ class ChatBlob:
         self.msg = msg
         self.page_prefix = ""
         self.page_postfix = ""
+
+    def __str__(self):
+        return f"ChatBlob('{self.title}', '{self.msg}')"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, obj):
+        return isinstance(obj, ChatBlob) and \
+               obj.title == self.title and \
+               obj.msg == self.msg and \
+               obj.page_prefix == self.page_prefix and \
+               obj.page_postfix == self.page_postfix
