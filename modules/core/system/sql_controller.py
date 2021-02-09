ea47a79968729b0ffe7473b74ac5d978edfbaae1
Tyrence <email1@jkbff.com>
2021-02-08 18:57:33 -0700

    added !sql files and !sql load commands

--- modules/core/system/sql_controller.py
+++ modules/core/system/sql_controller.py
@@ -9,6 +9,7 @@ class SqlController:
     def inject(self, registry):
         self.bot = registry.get_instance("bot")
         self.db = registry.get_instance("db")
+        self.text = registry.get_instance("text")
         self.command_alias_service = registry.get_instance("command_alias_service")
         self.getresp = registry.get_instance("translation_service").get_response
 
@@ -32,5 +33,28 @@ class SqlController:
         try:
             row_count = self.db.exec(sql)
             return self.getresp("module/system", "sql_exec_success", {"count": row_count})
+        except Exception as e:
+            return self.getresp("module/system", "sql_fail", {"error": str(e)})
+
+    @command(command="sql", params=[Const("files")], access_level="superadmin",
+             description="Show SQL files that have been loaded")
+    def sql_files_cmd(self, request, _):
+        data = self.db.query("SELECT file, version FROM db_version ORDER BY file ASC")
+
+        blob = ""
+        for row in data:
+            reload_link = ""
+            if row.file != "db_version":
+                reload_link = self.text.make_tellcmd("Reload", f"sql load {row.file}")
+            blob += f"{row.file} - {row.version} {reload_link}\n"
+
+        return ChatBlob("SQL Files (%d)" % len(data), blob)
+
+    @command(command="sql", params=[Const("load"), Any("sql_file")], access_level="superadmin",
+             description="Load or reload a SQL file")
+    def sql_load_cmd(self, request, _, file):
+        try:
+            self.db.load_sql_file(file, force_update=True)
+            return f"SQL file <highlight>{file}</highlight> has been loaded."
         except Exception as e:
             return self.getresp("module/system", "sql_fail", {"error": str(e)})
