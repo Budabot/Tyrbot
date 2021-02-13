16917c64a18fc2d589c33ccaa71eb42f468ec5b4
Tyrence <email1@jkbff.com>
2021-02-13 02:17:34 -0700

    updated version to 0.6-beta

--- core/tyrbot.py
+++ core/tyrbot.py
@@ -39,7 +39,7 @@ class Tyrbot:
         self.dimension = None
         self.last_timer_event = 0
         self.start_time = int(time.time())
-        self.version = "0.5-beta"
+        self.version = "0.6-beta"
         self.incoming_queue = FifoQueue()
         self.mass_message_queue = None
         self.conns = DictObject()
