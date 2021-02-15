729be1b41b3f797c5ef0d5e3f4ab169623584459
Tyrence <email1@jkbff.com>
2021-02-15 03:24:18 -0700

    updated wave counter for multiple conns

--- modules/standard/org/wave_counter_controller.py
+++ modules/standard/org/wave_counter_controller.py
@@ -1,3 +1,4 @@
+from core.conn import Conn
 from core.decorators import instance, event
 from core.public_channel_service import PublicChannelService
 
@@ -8,10 +9,6 @@ class WaveCounterController:
     CITY_TARGETED = [1001, 3]
 
     ALERT_TIMES = [105, 150, 90, 120, 120, 120, 120, 120, 120]
-
-    def __init__(self):
-        self.current_wave = None
-        self.scheduled_job_id = None
 
     def inject(self, registry):
         self.job_scheduler = registry.get_instance("job_scheduler")
@@ -25,24 +22,24 @@ class WaveCounterController:
         ext_msg = event_data.extended_message
         if ext_msg:
             if [ext_msg.category_id, ext_msg.instance_id] == self.CITY_TARGETED:
-                self.start_counter()
+                self.start_counter(event_data.conn)
 
-    def start_counter(self):
-        if self.scheduled_job_id:
-            self.job_scheduler.cancel_job(self.scheduled_job_id)
+    def start_counter(self, conn: Conn):
+        if conn.data.scheduled_job_id:
+            self.job_scheduler.cancel_job(conn.data.scheduled_job_id)
 
-        self.send_message("Wave counter started.")
-        self.scheduled_job_id = self.job_scheduler.delayed_job(self.timer_alert, self.ALERT_TIMES[0], 0)
+        self.send_message(f"Wave counter started for {conn.org_name or conn.char_name}.")
+        conn.data.scheduled_job_id = self.job_scheduler.delayed_job(self.timer_alert, self.ALERT_TIMES[0], conn, 0)
 
-    def timer_alert(self, t, wave_number):
+    def timer_alert(self, conn, t, wave_number):
         wave_number += 1
 
         if wave_number == 9:
             self.send_message("General incoming.")
-            self.scheduled_job_id = None
+            conn.data.scheduled_job_id = None
         else:
             self.send_message("Wave <highlight>%d</highlight> incoming." % wave_number)
-            self.scheduled_job_id = self.job_scheduler.scheduled_job(self.timer_alert, t + self.ALERT_TIMES[wave_number], wave_number)
+            conn.data.scheduled_job_id = self.job_scheduler.scheduled_job(self.timer_alert, t + self.ALERT_TIMES[wave_number], conn, wave_number)
 
     def send_message(self, msg):
         self.message_hub_service.send_message(self.MESSAGE_SOURCE, None, None, msg)
