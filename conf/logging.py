ce0d3fc37087c16a43d61d97a3a00e66250f7a3e
Tyrence <email1@jkbff.com>
2021-02-16 05:50:17 -0700

    added error.log

--- conf/logging.py
+++ conf/logging.py
@@ -14,6 +14,10 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(messag
 file_handler = logging.handlers.RotatingFileHandler("./logs/bot.log", maxBytes=20 * 1024 * 1024, backupCount=1000, encoding="utf-8")
 file_handler.setFormatter(formatter)
 
+err_file_handler = logging.handlers.RotatingFileHandler("./logs/error.log", maxBytes=20 * 1024 * 1024, backupCount=1000, encoding="utf-8")
+err_file_handler.setFormatter(formatter)
+err_file_handler.setLevel(logging.ERROR)
+
 console_out = logging.StreamHandler(sys.stdout)
 console_out.setFormatter(formatter)
 console_out.addFilter(FilterInfo())
@@ -26,7 +30,4 @@ logging.root.setLevel(logging.INFO)
 logging.root.addHandler(file_handler)
 logging.root.addHandler(console_out)
 logging.root.addHandler(console_err)
-
-# reduce discord spam
-#logging.getLogger("websockets").setLevel(logging.INFO)
-#logging.getlogger("discord").setLevel(logging.INFO)
+logging.root.addHandler(err_file_handler)
