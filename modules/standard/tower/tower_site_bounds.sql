cb6230c92889fa3f8bcaee55e12ec51d95e03581
Tyrence <email1@jkbff.com>
2021-07-30 17:14:37 -0600

    added bounding boxes for galway shire, holes in the wall, and stret west bank

--- modules/standard/tower/tower_site_bounds.sql
+++ modules/standard/tower/tower_site_bounds.sql
@@ -21,3 +21,29 @@ INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_
 INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (560, 9, 2610, 630, 2900, 380);
 INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (655, 6, 3941, 1146, 4651, 628);
 INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (655, 8, 4093, 4659, 207, 628);
+
+-- galway shire - done
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (687, 1, 372, 2039, 577, 1832);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (687, 2, 293, 1381, 438, 1237);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (687, 3, 815, 1346, 1002, 1115);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (687, 4, 697, 978, 850, 886);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (687, 5, 254, 664, 460, 517);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (687, 6, 545, 662, 710, 464);
+
+-- holes in the wall - done
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (791, 1, 252, 2098, 580, 1940);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (791, 2, 571, 1623, 782, 1430);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (791, 3, 146, 1152, 299, 994);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (791, 4, 497, 970, 947, 699);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (791, 5, 653, 602, 902, 342);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (791, 6, 266, 427, 454, 229);
+
+-- stret west bank - done
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 1, 1618, 3222, 1775, 2950);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 2, 2171, 3232, 2390, 2543);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 3, 1573, 2903, 1815, 2663);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 4, 1860, 2907, 2105, 2675);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 5, 1250, 2400, 1960, 1580);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 6, 1153, 1387, 1467, 982);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 7, 480, 1475, 822, 874);
+INSERT INTO tower_site_bounds (playfield_id, site_number, x_coord1, y_coord1, x_coord2, y_coord2) VALUES (790, 8, 2140, 1467, 2411, 861);
