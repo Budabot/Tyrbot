f08bbac7a0a6a2ccdcffdff76ff0e57540cf2ca1
Tyrence <email1@jkbff.com>
2021-12-10 22:49:47 -0700

    show org name instead of [Org] in relayed messages

--- modules/standard/org/org_channel_controller.py
+++ modules/standard/org/org_channel_controller.py
@@ -12,7 +12,6 @@ from modules.core.org_members.org_member_controller import OrgMemberController
 @instance()
 class OrgChannelController:
     MESSAGE_SOURCE = "org_channel"
-    ORG_CHANNEL_PREFIX = "[Org]"
 
     def __init__(self):
         self.logger = Logger(__name__)
@@ -36,8 +35,6 @@ class OrgChannelController:
             ["private_channel", "discord", "websocket_relay", "broadcast", "raffle", "cloak_reminder", "wave_counter", "shutdown_notice", "raid", "tower_attacks", "timers"],
             [self.MESSAGE_SOURCE])
 
-        self.setting_service.register(self.module_name, "prefix_org_priv", True, BooleanSettingType(), "Should the prefix [org] be displayed in relayed messages")
-
     def handle_incoming_relay_message(self, ctx):
         for _id, conn in self.bot.get_conns(lambda x: x.is_main and x.org_id):
             self.bot.send_org_message(ctx.formatted_message, conn=conn)
@@ -47,6 +44,8 @@ class OrgChannelController:
         if self.bot.get_conn_by_char_id(event_data.char_id) or self.ban_service.get_ban(event_data.char_id):
             return
 
+        org_prefix = self.get_org_prefix(event_data.conn)
+
         if event_data.extended_message:
             message = event_data.extended_message.get_message()
         else:
@@ -54,16 +53,16 @@ class OrgChannelController:
 
         if event_data.char_id == 4294967295 or event_data.char_id == 0:
             sender = None
-            formatted_message = "{org} {msg}".format(org=self.ORG_CHANNEL_PREFIX,
+            formatted_message = "{org} {msg}".format(org=org_prefix,
                                                      msg=message)
         else:
             sender = DictObject({"char_id": event_data.char_id, "name": event_data.name})
-            formatted_message = "{org} {char}: {msg}".format(org=self.ORG_CHANNEL_PREFIX,
+            formatted_message = "{org} {char}: {msg}".format(org=org_prefix,
                                                              char=self.text.make_charlink(event_data.name),
                                                              msg=message)
 
         self.bot.send_message_to_other_org_channels(formatted_message, from_conn=event_data.conn)
-        self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, self.ORG_CHANNEL_PREFIX, message)
+        self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, org_prefix, message)
 
     @event(event_type=OrgMemberController.ORG_MEMBER_LOGON_EVENT, description="Notify when org member logs on")
     def org_member_logon_event(self, event_type, event_data):
@@ -79,7 +78,7 @@ class OrgChannelController:
 
             for _id, conn in self.bot.get_conns(lambda x: x.is_main and x.org_id):
                 self.bot.send_org_message(msg, conn=conn)
-            self.message_hub_service.send_message(self.MESSAGE_SOURCE, None, self.ORG_CHANNEL_PREFIX, msg)
+            self.message_hub_service.send_message(self.MESSAGE_SOURCE, None, self.get_org_prefix(event_data.conn), msg)
 
     @event(event_type=OrgMemberController.ORG_MEMBER_LOGOFF_EVENT, description="Notify when org member logs off")
     def org_member_logoff_event(self, event_type, event_data):
@@ -91,11 +90,12 @@ class OrgChannelController:
 
             for _id, conn in self.bot.get_conns(lambda x: x.is_main and x.org_id):
                 self.bot.send_org_message(msg, conn=conn)
-            self.message_hub_service.send_message(self.MESSAGE_SOURCE, None, self.ORG_CHANNEL_PREFIX, msg)
+            self.message_hub_service.send_message(self.MESSAGE_SOURCE, None, self.get_org_prefix(event_data.conn), msg)
 
     @event(event_type=PublicChannelService.ORG_CHANNEL_COMMAND_EVENT, description="Relay commands from the org channel to the relay hub", is_system=True)
     def outgoing_org_message_event(self, event_type, event_data):
-        msg = self.ORG_CHANNEL_PREFIX + " "
+        org_prefix = self.get_org_prefix(event_data.conn)
+        msg = org_prefix + " "
         sender = None
         if event_data.name:
             msg += self.text.make_charlink(event_data.name) + ": "
@@ -109,12 +109,15 @@ class OrgChannelController:
                 for page in pages:
                     message = msg + page
                     self.bot.send_message_to_other_org_channels(message, from_conn=event_data.conn)
-                    self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, self.ORG_CHANNEL_PREFIX, page)
+                    self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, org_prefix, page)
             else:
                 message = msg + event_data.message.title
                 self.bot.send_message_to_other_org_channels(message, from_conn=event_data.conn)
-                self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, self.ORG_CHANNEL_PREFIX, event_data.message.title)
+                self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, org_prefix, event_data.message.title)
         else:
             message = msg + event_data.message
             self.bot.send_message_to_other_org_channels(message, from_conn=event_data.conn)
-            self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, self.ORG_CHANNEL_PREFIX, event_data.message)
+            self.message_hub_service.send_message(self.MESSAGE_SOURCE, sender, org_prefix, event_data.message)
+
+    def get_org_prefix(self, conn):
+        return "[%s]" % conn.get_org_name()
