6f0ddd4452b1d217573f66138595a0249c617b98
Tyrence <email1@jkbff.com>
2022-01-22 15:15:42 -0700

    added !assist rem

--- modules/standard/raid/assist_controller.py
+++ modules/standard/raid/assist_controller.py
@@ -1,5 +1,5 @@
 from core.decorators import instance, command
-from core.command_param_types import Any, Const
+from core.command_param_types import Any, Const, Options
 from .leader_controller import LeaderController
 
 
@@ -30,6 +30,21 @@ class AssistController:
             request.conn.data.assist = None
             return "Assist targets have been cleared."
 
+    @command(command="assist", params=[Options(["rem", "remove"]), Any("assist_targets")], access_level="all",
+             description="Remove one or more assist targets", sub_command="modify", extended_description="Multiple assist targets should be space-delimited")
+    def assist_rem_command(self, request, _, assist_targets):
+        targets = assist_targets.split(" ")
+
+        if not self.leader_controller.can_use_command(request.sender.char_id, request.conn):
+            return LeaderController.NOT_LEADER_MSG
+        else:
+            assist = request.conn.data.get("assist") or []
+            for target in targets:
+                target = target.capitalize()
+                if target in assist:
+                    self.remove_assist(target, request.conn)
+            return self.get_assist_output(request.conn)
+
     @command(command="assist", params=[Const("add", is_optional=True), Any("assist_targets")], access_level="all",
              description="Add one or more assist targets", sub_command="modify", extended_description="Multiple assist targets should be space-delimited")
     def assist_set_command(self, request, _, assist_targets):
@@ -51,6 +66,11 @@ class AssistController:
             assist.append(target)
         else:
             conn.data.assist = [target]
+
+    def remove_assist(self, target, conn):
+        assist = conn.data.get("assist")
+        if assist:
+            assist.remove(target)
 
     def get_assist_output(self, conn):
         assist = conn.data.get("assist")
