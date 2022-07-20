d984c1589bda0aaa9b658d303799e758dd7e34cb
Tyrence <email1@jkbff.com>
2022-07-20 11:36:46 -0600

    add track messages to message_hub

--- modules/standard/track/track_controller.py
+++ modules/standard/track/track_controller.py
@@ -8,6 +8,8 @@ from core.chat_blob import ChatBlob
 
 @instance()
 class TrackController:
+    MESSAGE_SOURCE = "track"
+
     def __init__(self):
         self.tracked_char_ids = set()
 
@@ -17,6 +19,11 @@ class TrackController:
         self.buddy_service = registry.get_instance("buddy_service")
         self.util = registry.get_instance("util")
         self.text = registry.get_instance("text")
+        self.character_service = registry.get_instance("character_service")
+        self.message_hub_service = registry.get_instance("message_hub_service")
+
+    def pre_start(self):
+        self.message_hub_service.register_message_source(self.MESSAGE_SOURCE)
 
     def start(self):
         self.db.exec("CREATE TABLE IF NOT EXISTS track (char_id INT NOT NULL PRIMARY KEY, added_by_char_id INT NOT NULL, created_at INT NOT NULL)")
@@ -71,10 +78,18 @@ class TrackController:
         if self.is_tracked_char(event_data.char_id):
             self.add_track_info(event_data.char_id, "logon")
 
+            if self.bot.is_ready():
+                char_name = self.character_service.resolve_char_to_name(event_data.char_id)
+                self.message_hub_service.send_message(self.MESSAGE_SOURCE, None, "[Track]", f"Character <highlight>{char_name}</highlight> has logged on.")
+
     @event(event_type=BuddyService.BUDDY_LOGOFF_EVENT, description="Record when a tracked char logs off", is_system=True)
     def buddy_logoff_event(self, event_type, event_data):
         if self.is_tracked_char(event_data.char_id):
             self.add_track_info(event_data.char_id, "logoff")
+
+            if self.bot.is_ready():
+                char_name = self.character_service.resolve_char_to_name(event_data.char_id)
+                self.message_hub_service.send_message(self.MESSAGE_SOURCE, None, "[Track]", f"Character <highlight>{char_name}</highlight> has logged off.")
 
     @event(event_type="connect", description="Add tracked characters as buddies", is_system=True)
     def connect_event(self, event_type, event_data):
