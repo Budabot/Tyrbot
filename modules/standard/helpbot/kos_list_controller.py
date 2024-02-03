51bf483356377ae9033b3e3cadc0179942f337e8
cap5lut <florian.pattke@gmail.com>
2024-02-03 21:13:39 +0100

    makes responses more concise and fetches PoRK only on the `!kos add` command instead of in the `!kos` command

--- modules/standard/helpbot/kos_list_controller.py
+++ modules/standard/helpbot/kos_list_controller.py
@@ -1,8 +1,13 @@
+from core.buddy_service import BuddyService
 from core.chat_blob import ChatBlob
 from core.command_param_types import Character, Const, Options, Any
+from core.db import DB
 from core.decorators import instance, command
+from core.lookup.pork_service import PorkService
 from core.sender_obj import SenderObj
 from core.standard_message import StandardMessage
+from core.text import Text
+from core.tyrbot import Tyrbot
 import time
 
 @instance()
@@ -11,43 +16,40 @@ class KosListController:
         pass
 
     def inject(self, registry):
-        self.bot = registry.get_instance("bot")
-        self.buddy_service = registry.get_instance("buddy_service")
-        self.db = registry.get_instance("db")
-        self.pork_service = registry.get_instance("pork_service")
+        self.bot: Tyrbot = registry.get_instance("bot")
+        self.buddy_service: BuddyService = registry.get_instance("buddy_service")
+        self.db: DB = registry.get_instance("db")
+        self.pork_service: PorkService = registry.get_instance("pork_service")
+        self.text: Text = registry.get_instance("text")
 
     def start(self):
         self.db.exec("CREATE TABLE IF NOT EXISTS kos_list (target_id INT PRIMARY KEY, submitter_id INT NOT NULL,"
                      " submitted_at INT NOT NULL, reason VARCHAR(2000) NOT NULL DEFAULT '')")
-    
+
     @command(command="kos", params=[], access_level="member", description="Shows the kill-on-sight list")
-    def kos_list_show_cmd(self, request):
-        entries = self.db.query("SELECT kos_list.target_id AS target_id, p1.name AS target_name, "
-                                "p2.name AS submitter_name, kos_list.submitted_at AS submitted_at, kos_list.reason AS reason "
+    def kos_list_show_cmd2(self, request):
+        entries = self.db.query("SELECT p1.char_id AS char_id, p1.name AS name, p1.level AS level, p1.ai_level AS ai_level, p1.profession AS profession, p1.faction AS faction, "
+                                "p1.org_name AS org_name, p1.org_rank_name AS org_rank_name, "
+                                "p2.name AS kos_submitter_name, kos_list.reason AS kos_reason, kos_list.submitted_at AS kos_submitted_at "
                                 "FROM kos_list "
                                 "LEFT JOIN player p1 ON kos_list.target_id = p1.char_id "
                                 "LEFT JOIN player p2 ON kos_list.submitter_id = p2.char_id "
-                                "ORDER BY kos_list.submitted_at DESC")
+                                "ORDER BY p1.name ASC")
         count = len(entries)
         
         if count == 0:
-            return "The kill-on-sight list is empty"
+            return "The kill-on-sight list is empty."
         
         blob = ""
         for entry in entries:
-            target_info = self.pork_service.get_character_info(entry.target_name)
-            blob += "<highlight>%s</highlight> " % entry.target_name
-            if target_info is not None:
-                blob += "%s (%d/<green>%d</green>) " % (target_info.profession, target_info.level, target_info.ai_level)
-            if self.buddy_service.is_online(entry.target_id):
-                blob += "<green>online</green> "
-            if (len(entry.reason) == 0):
-                blob += "added by %s" % entry.submitter_name
+            blob += self.text.format_char_info(entry, self.buddy_service.is_online(entry.char_id))
+            if (len(entry.kos_reason) == 0):
+                blob += " (by %s)" % entry.kos_submitter_name
             else:
-                blob += "added by %s: %s" % (entry.submitter_name, entry.reason)
+                blob += " (by %s: %s)" % (entry.kos_submitter_name, entry.kos_reason)
             blob += "\n"
         
-        return ChatBlob("Kill-on-sight list (%d)" % count, blob)
+        return ChatBlob("Kill-on-sight List (%d)" % count, blob)
         
     @command(command="kos", params=[Const("add"), Character("character"), Any("reason", is_optional=True)], access_level="member",
              description="Adds a character to the kill-on-sight list")
@@ -60,7 +62,10 @@ class KosListController:
         if result == 0:
             return "Character <highlight>%s</highlight> is already on the kill-on-sight list." % target.name
         
-        return "Character <highlight>%s</highlight> has been added to the kill-on-sight list." % target.name
+        # send the reply and afterwards trigger an info fetch from PoRK to update the database
+        # the reply needs to be sent first to avoid the unnecessary delay from the PoRK request
+        request.reply("Character <highlight>%s</highlight> has been added to the kill-on-sight list." % target.name)
+        self.pork_service.get_character_info(target.name)
     
     @command(command="kos", params=[Options(["rem", "remove"]), Character("character")], sub_command="remove", access_level="moderator",
              description="Removes a character from the kill-on-sight list")
