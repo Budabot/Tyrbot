07d87c4901ffc620b9d3fd9fe6986558e3a9d74c
Tyrence <email1@jkbff.com>
2025-02-09 23:47:58 -0700

    FeatureFlags.AUTO_UNFREEZE_ACCOUNTS is now enabled by default

--- conf/config_template.py
+++ conf/config_template.py
@@ -29,16 +29,16 @@ config = DictObject({
   },
 
   "features": {
-    "text_formatting_v2": False,
-    "use_tower_api": True,
-    "force_large_messages_from_slaves": True,
-    "ignore_failed_bots_on_login": False,
-    "auto_unfreeze_accounts": False,
+    "text_formatting_v2": False,              # when enabled, uses an alternate text formatting implementation (not recommended)
+    "use_tower_api": True,                    # when enabled, will use the Tower API configured in the bot rather than the bot's local database (recommended)
+    "force_large_messages_from_slaves": True, # when enabled, the bot will send large messages from multiple slave bots rather than the main bot
+    "ignore_failed_bots_on_login": False,     # when enabled, the bot will continue logging in even if some of the bots in the config fail, as long as the login for the first bot in the config succeeds
+    "auto_unfreeze_accounts": True,           # when enabled, the bot will automatically unfreeze bot accounts by logging into the Funcom website
   },
 
   "module_paths": [
-    "modules/core",
-    "modules/standard",
-    "modules/custom",
+    "modules/core",     # core modules necessary for running/configuring/managing the bot
+    "modules/standard", # standard modules for managing an org, and other common functions such as looking up items
+    "modules/custom",   # custom, user-provided, or third-party modules should be put in this directory
   ]
 })
